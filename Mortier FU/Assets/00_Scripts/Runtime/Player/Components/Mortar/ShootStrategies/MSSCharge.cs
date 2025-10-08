a3d62321d5548c269432758ed29b5df847664ec9
Antoine VOLLET <a.vollet@rubika-edu.com>
2025-10-08 12:28:46 +0200

    Mortar: Added a new Charge strategy for shooting

--- Mortier FU/Assets/00_Scripts/Runtime/Player/Components/Mortar/ShootStrategies/MSSCharge.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Player/Components/Mortar/ShootStrategies/MSSCharge.cs
@@ -1,8 +1,92 @@
-﻿namespace MortierFu
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace MortierFu
 {
+    // Either relative or absolute: meaning the charge takes longer to reach max if the mortar range is higher (RELATIVE)
     public class MSSCharge : MortarShootStrategy
     {
-        public MSSCharge(Mortar mortar) : base(mortar)
-        { }
+        private float _chargeSpeed;
+        
+        private bool _isCharging;
+        private float _currentCharge;
+        private Vector2 _currentAimInput;
+
+        public MSSCharge(Mortar mortar, InputAction aimAction, InputAction shootAction, float chargeSpeed = 1.0f) :
+            base(mortar, aimAction, shootAction)
+        {
+            _chargeSpeed = chargeSpeed;
+            _currentCharge = 0.0f;
+        }
+        
+        public void SetChargeSpeed(float speed) => _chargeSpeed = speed;
+        
+        public override void Initialize()
+        {
+            var aimWidget = mortar.AimWidget;
+
+            aimWidget.IsActive = false;
+            aimWidget.AttachedToTarget = true;
+            aimWidget.Target = mortar.transform;
+            aimWidget.RelativePosition = Vector3.zero;
+            aimWidget.Show();
+            
+            // Bind to actions
+            aimAction.performed += OnAiming;
+            shootAction.started += BeginCharging;
+            shootAction.canceled += EndCharging;
+        }
+
+        public override void DeInitialize()
+        {
+            // Unbind from actions
+            aimAction.performed -= OnAiming;
+            shootAction.started -= BeginCharging;
+            shootAction.canceled -= EndCharging;
+        }
+
+        public override void Update()
+        {
+            if (!_isCharging) return;
+            
+            _currentCharge += Time.deltaTime * _chargeSpeed;
+            _currentCharge = Mathf.Clamp01(_currentCharge); 
+            
+            Vector3 newPos = new Vector3(_currentAimInput.x, 0.0f, _currentAimInput.y) * (mortar.ShotRange.Value * _currentCharge);
+            mortar.AimWidget.RelativePosition = newPos;
+        }
+        
+        private void BeginCharging(InputAction.CallbackContext obj)
+        {
+            if (_isCharging) return;
+            
+            _currentCharge = 0.0f;
+            _isCharging = true;
+
+            var aimWidget = mortar.AimWidget;
+            aimWidget.IsActive = true;
+            aimWidget.RelativePosition = Vector3.zero;
+        }
+        
+        private void EndCharging(InputAction.CallbackContext obj)
+        {
+            if (!_isCharging)
+                return;
+            
+            _isCharging = false;
+            mortar.AimWidget.IsActive = false;
+            
+            mortar.Shoot();
+        }
+        
+        private void OnAiming(InputAction.CallbackContext ctx)
+        {
+            var aimInput = ctx.ReadValue<Vector2>();
+
+            if (aimInput.sqrMagnitude < k_minAimInputLength)
+                return;
+
+            _currentAimInput = aimInput.normalized;
+        }
     }
 }
--- Mortier FU/Assets/00_Scripts/Runtime/Player/Components/Mortar/ShootStrategies/MSSDirectionAutoTarget.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Player/Components/Mortar/ShootStrategies/MSSDirectionAutoTarget.cs
@@ -1,8 +1,10 @@
-﻿namespace MortierFu
+﻿using UnityEngine.InputSystem;
+
+namespace MortierFu
 {
     public class MSSDirectionAutoTarget : MortarShootStrategy
     {
-        public MSSDirectionAutoTarget(Mortar mortar) : base(mortar)
+        public MSSDirectionAutoTarget(Mortar mortar, InputAction aimAction, InputAction shootAction) : base(mortar, aimAction, shootAction)
         { }
     }
 }
