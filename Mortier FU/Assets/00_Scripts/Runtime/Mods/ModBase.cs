35071f0d5b65a14d04c76ebc3b2ecdcc8b184b80
Ase <tdruesne05@gmail.com>
2025-10-10 17:04:46 +0200

    mod uwu

---
+++ Mortier FU/Assets/00_Scripts/Runtime/Mods/ModBase.cs
@@ -0,0 +1,12 @@
+﻿// Classe de base pour tous les mods
+
+namespace MortierFu
+{
+    public abstract class ModBase
+    {
+        public abstract void Initialize();
+        public virtual void DeInitialize() { }
+
+        public virtual void ModUpdate() { }
+    }
+}
