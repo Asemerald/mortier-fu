a915c2e9d616a5efa3b1c7e4c89f2751edd3e509
Ase <tdruesne05@gmail.com>
2025-10-14 14:13:15 +0200

    AudioService WIP

--- Mortier FU/Assets/00_Scripts/Runtime/GameManager/AudioService.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/GameManager/AudioService.cs
@@ -1,7 +1,23 @@
-﻿namespace MortierFu
+﻿using MortierFu.Shared;
+
+namespace MortierFu
 {
-    public class AudioService
+    public class AudioService : IGameService
     {
+
+        public void Initialize()
+        {
+            Logs.Log("[AudioService] Initialized!");
+        }
+
+        public void Tick()
+        {
+        }
         
+        public void Dispose()
+        {
+            
+        }
+
     }
 }
