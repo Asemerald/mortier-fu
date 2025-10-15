7ddbed010cd34298958486ac73c26751bfcde415
Antoine VOLLET <a.vollet@rubika-edu.com>
2025-10-15 10:37:57 +0200

    Fix AugmentFactory to use the C# type of the SerializeType

--- Mortier FU/Assets/00_Scripts/Runtime/Augments/AugmentFactory.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Augments/AugmentFactory.cs
@@ -9,7 +9,7 @@
                 throw new System.ArgumentNullException(nameof(augment), "Augment data or type cannot be null");
             }
 
-            var augmentInstance = (IAugment)System.Activator.CreateInstance(augment.AugmentType, augment, owner);
+            var augmentInstance = (IAugment)System.Activator.CreateInstance(augment.AugmentType.Type, augment, owner);
             return augmentInstance;
         }
     }
