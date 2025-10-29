bbeb4a4af9b2ce6fb5a2a01d8b0825e5cca0e8d0
Enzo Dufour <enzo.dufour05@gmail.com>
2025-10-29 16:41:57 +0100

    Do not take again again again

--- Mortier FU/Assets/00_Scripts/Runtime/UI/InGame/ScorePanel.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/UI/InGame/ScorePanel.cs
@@ -1,59 +1,22 @@
-using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
 namespace MortierFu
 {
     public class ScorePanel : MonoBehaviour
     {
-        [SerializeField] private Transform scorePanelGroup;
-        [SerializeField] private GameObject scoreTextPrefab;
+        [SerializeField] private TextMeshProUGUI _scoreText;
+        private PlayerTeam _team;
+
+        public void Initialize(PlayerTeam team)
+        {
+            _team = team;
+            UpdateData();
+        }
         
-        private Dictionary<Cnc, TMP_Text> playerScoreTexts = new();
-        
-        public void Init(List<Cnc> players)
+        public void UpdateData()
         {
-            playerScoreTexts.Clear();
-
-            foreach (Transform child in scorePanelGroup)
-            {
-                Destroy(child.gameObject);
-            }
-
-            foreach (var player in players)
-            {
-                var go = Instantiate(scoreTextPrefab, scorePanelGroup);
-                var text = go.GetComponent<TMP_Text>();
-                
-                playerScoreTexts[player] = text;
-                UpdateScore(player);
-            }
-            
-            Hide();
-        }
-
-        private void UpdateScore(Cnc player)
-        {
-            if (playerScoreTexts.TryGetValue(player, out var scoreText))
-            {
-                scoreText.text = $"Joueur {player.PlayerNumber + 1} : {player.Score}";
-            }
-        }
-
-        public void UpdateAllScores()
-        {
-            foreach (var player in playerScoreTexts.Keys)
-                UpdateScore(player);
-        }
-
-        public void Show()
-        {
-            gameObject.SetActive(true);
-        }
-
-        public void Hide()
-        {
-            gameObject.SetActive(false);
+            _scoreText.text = $"Score: {_team.Score}";
         }
     }
 }
