f336ab82e9c7524593507be4e8dced975c332db4
EliotTranson <e.transon@rubika-edu.com>
2025-10-29 16:43:32 +0100

    TEMP_CameraHandler + PREF_Camera UPDATE

--- Mortier FU/Assets/00_Scripts/Runtime/Core/Camera/TEMP_CameraHandler.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Core/Camera/TEMP_CameraHandler.cs
@@ -10,34 +10,75 @@ public class TEMP_CameraHandler : MonoBehaviour
     [Space(10)]
     
     [SerializeField] private CinemachineTargetGroup _targetGroup;
+    [SerializeField] private Transform _target;
+    
     private float playerDist;
-
-    [SerializeField] private AnimationCurve fovCurve;
+    private Vector3 hideTarget;
+    
+    private float targetFov, targetOrthoSize;
+    private float hideFov, hideOrthoSize;
 
     private void Start()
     {
         cameraShake = GetComponent<TEMP_CameraShake>();
+        targetFov = 60;
+        targetOrthoSize = 18;
+        hideFov = 60;
+        hideOrthoSize = 18;
     }
 
     private void Update()
     {
-        var dist = Vector3.Distance(_targetGroup.transform.position, _targetGroup.Targets[0].Object.position);
-        playerDist = Mathf.Lerp(playerDist, dist, Time.deltaTime * 4);
-        Logs.Log($"Distance : {playerDist}");
+        MovementUpdate();
+        TargetUpdate();
+    }
+
+    private void MovementUpdate()
+    {
+        float dist;
+        if (!_targetGroup.IsEmpty)
+        {
+            dist = Vector3.Distance(_targetGroup.transform.position, _targetGroup.Targets[0].Object.position);
+            playerDist = Mathf.Lerp(playerDist, dist, Time.deltaTime * 4);
+        }
+        else
+        {
+            playerDist = 0;
+        }
+        Logs.Log($"Distance : {targetOrthoSize}");
         
         switch (playerDist)
         {
             case < 7 :
                 break;
             case < 15 :
-                cinemachineCamera.Lens.OrthographicSize = 18 - ((15 - playerDist)/1.6f) + cameraShake.addedFOV;
-                _targetGroup.enabled = true;
+                targetOrthoSize = 18 - ((15 - playerDist) / 1.6f);
+                targetFov = 60  - (15 - playerDist);
                 break;
             default :
-                cinemachineCamera.Lens.OrthographicSize = 18 + cameraShake.addedFOV;
-                _targetGroup.enabled = false;
-                _targetGroup.transform.position = Vector3.Lerp(_targetGroup.transform.position, Vector3.zero, Time.deltaTime);
+                targetOrthoSize = 18;
+                targetFov = 60;
                 break;
+        }
+
+        hideFov = Mathf.Lerp(hideFov, targetFov, Time.deltaTime * 1);
+        hideOrthoSize = Mathf.Lerp(hideOrthoSize, targetOrthoSize, Time.deltaTime * 1);
+        
+        cinemachineCamera.Lens.OrthographicSize = hideOrthoSize + cameraShake.addedFOV;     //ORTHO
+        cinemachineCamera.Lens.FieldOfView = hideFov + cameraShake.addedFOV;                //PERSPECTIVE
+    }
+
+    private void TargetUpdate()
+    {
+        _target.position = Vector3.Lerp(_target.position, hideTarget, Time.deltaTime);
+        
+        if (playerDist < 15)
+        {
+            hideTarget = _targetGroup.transform.position;
+        }
+        else
+        {
+            hideTarget = Vector3.zero;
         }
     }
 }
