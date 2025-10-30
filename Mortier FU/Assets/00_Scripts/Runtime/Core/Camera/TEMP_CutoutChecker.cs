c758d0d46116dc6a724f78d72d210a674b9ab1f1
EliotTranson <e.transon@rubika-edu.com>
2025-10-30 20:17:12 +0100

    CutoutChecker V1 PROOOOOPRE

---
+++ Mortier FU/Assets/00_Scripts/Runtime/Core/Camera/TEMP_CutoutChecker.cs
@@ -0,0 +1,46 @@
+using System;
+using MortierFu.Shared;
+using PrimeTween;
+using UnityEngine;
+
+public class TEMP_CutoutChecker : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private LayerMask _lm;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+        
+        //Does the ray intersects with the mesh?
+        if (Physics.Raycast(_camera.transform.position, (transform.position - _camera.transform.position).normalized, out hit, 1000))
+        {
+            Logs.Log(hit.transform.name);
+            
+            if (hit.transform != transform.parent.parent)
+            {
+                if (transform.localScale == Vector3.zero)
+                {
+                    Tween.Scale(transform, transform.localScale, Vector3.one * 4, 0.5f);
+                }
+            }
+            else
+            {
+                if (transform.localScale == Vector3.one * 4)
+                {
+                    Tween.Scale(transform, transform.localScale, Vector3.zero, 0.5f);
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(_camera.transform.position, (transform.position - _camera.transform.position).normalized * 1000);
+    }
+}
