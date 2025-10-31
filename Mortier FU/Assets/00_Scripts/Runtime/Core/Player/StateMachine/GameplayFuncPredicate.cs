2a6eb25c0143e19684a4caa0dd4ccc3b7673684a
Antoine VOLLET <a.vollet@rubika-edu.com>
2025-10-31 12:45:33 +0100

    GameplayActions for the character
    Prevents shooting while selecting the augments

---
+++ Mortier FU/Assets/00_Scripts/Runtime/Core/Player/StateMachine/GameplayFuncPredicate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MortierFu
+{
+    public class GameplayFuncPredicate : IPredicate
+    {
+        private readonly Func<bool> _func;
+        
+        public GameplayFuncPredicate(Func<bool> func)
+        {
+            _func = func;
+        }
+        
+        public bool Evaluate() => PlayerCharacter.AllowGameplayActions && _func.Invoke();
+    }
+}
