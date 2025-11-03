2e97e82a5762641d8f551791d6a7ffd6440281f2
Pediluv <s.sindt@rubika-edu.com>
2025-11-03 15:35:00 +0100

    Prefabs : LD5 #1

--- Mortier FU/Assets/00_Scripts/Runtime/TEMP_LD_Elements/Movable.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/TEMP_LD_Elements/Movable.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using NUnit.Framework.Constraints;
 using UnityEngine;
 
@@ -7,6 +8,8 @@ public class Movable : MonoBehaviour
     public Transform target;
     public float speed;
     private Vector3 startingPoint;
+    private bool canMove = true;
+    public float waitTime = 1.5f;
 
     private bool hasbeenActivated =false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,13 +22,14 @@ public class Movable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (target != null & isAutomatic || hasbeenActivated & target != null & !isAutomatic)
+        if (target != null & isAutomatic & canMove || hasbeenActivated & target != null & !isAutomatic)
         {
             gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, target.position, speed/1000);
             if (transform.position == target.position)
             {
                 (target.position,startingPoint) = (startingPoint,target.position);
                 hasbeenActivated = false;
+                StartCoroutine(Wait());
             }
         }
         else
@@ -37,6 +41,12 @@ public class Movable : MonoBehaviour
     public void InteratableMove()
     {
         hasbeenActivated = true;
+    }
+
+    private IEnumerator Wait()
+    {
+        canMove = !canMove;
+        yield return new WaitForSeconds(waitTime);
     }
     
 }
