d832ed5152db4ca792bb8eb546f44811abdd57e1
Antoine VOLLET <a.vollet@rubika-edu.com>
2025-11-03 16:39:49 +0100

    LootTable<T> + AugmentLibrary

---
+++ Mortier FU/Assets/00_Scripts/Editor/CustomEditors/DA_AugmentLibraryEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+
+namespace MortierFu.Editor
+{
+    [CustomEditor(typeof(DA_AugmentLibrary), true)]
+    public class DA_AugmentLibraryEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            var holder = target as DA_AugmentLibrary;
+            float totalWeight = 0f;
+            
+            if (holder && holder.AugmentEntries != null && holder.AugmentEntries.Count > 0)
+            {
+                foreach (var entry in holder.AugmentEntries)
+                {
+                    totalWeight += entry.Weight;
+                }
+            }
+            
+            EditorGUILayout.LabelField("Total Weight", totalWeight.ToString("F2"));
+        }
+    }
+}
---
+++ Mortier FU/Assets/00_Scripts/Runtime/Systems/LootTableConfig.cs
@@ -0,0 +1,7 @@
+﻿public class LootTableConfig
+{
+    // If enabled, the loot bag can contain duplicate items.
+    public bool AllowDuplicates = false;
+    // If enabled, the pulled item will be removed from the loot bag.
+    public bool RemoveOnPull = false;
+}
