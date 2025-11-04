99260c77932c63e8145af20090e48901418be20b
Enzo Dufour <enzo.dufour05@gmail.com>
2025-11-04 17:21:55 +0100

    Start augments

---
+++ Mortier FU/Assets/00_Scripts/Runtime/Augments/Stats/AGM_FastReload.cs
@@ -0,0 +1,18 @@
+﻿namespace MortierFu.Stats
+{
+    public class AGM_FastReload : AugmentBase
+    {
+        public AGM_FastReload(DA_Augment augmentData, PlayerCharacter owner) : base(augmentData, owner)
+        { }
+
+        public override void Initialize()
+        {
+            stats.FireRate.AddModifier(new StatModifier(-0.15f, StatModType.PercentMult, this));
+        }
+        
+        public override void DeInitialize()
+        {
+            stats.FireRate.RemoveAllModifiersFromSource(this);
+        }
+    }
+}
