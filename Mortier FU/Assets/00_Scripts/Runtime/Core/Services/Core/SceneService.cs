fdcabeb80513a90c0a9203e81cacb8bc921da6e2
Padrox <a.vollet@rubika-edu.com>
2025-11-12 21:31:49 +0100

    Refacto SceneService for UniTask
    UniTask advices to not use .ToUniTask() on AsyncOperation !

--- Mortier FU/Assets/00_Scripts/Runtime/Core/Services/Core/SceneService.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Core/Services/Core/SceneService.cs
@@ -1,52 +1,52 @@
 using Cysharp.Threading.Tasks;
 using MortierFu.Shared;
-using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace MortierFu
 {
     public class SceneService : IGameService
     {
-        private GameObject _loadingGO;
+        private UnityEngine.GameObject _loadingGO;
 
         public void ShowLoadingScreen() => _loadingGO.SetActive(true);
         public void HideLoadingScreen() => _loadingGO.SetActive(false);
         
-        public async UniTask LoadScene(string sceneName, bool active = false)
-        {
-            var handle = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
-            if (handle == null)
-            {
-                Logs.LogError($"[SceneService]: Encountered an error trying to load {sceneName} !");
+        public async UniTask LoadScene(string sceneName, bool active = false) {
+            Logs.Log($"[SceneService]: Loading {sceneName}...");
+            var handle =  SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+            if (handle == null) {
+                Logs.LogError($"[SceneService]: Failed to load the following scene {sceneName} !");
                 return;
             }
             
-            await handle.ToUniTask();
+            await handle;
+            Logs.Log($"[SceneService]: Successfully loaded {sceneName}.");
 
-            if (!active) return;
-                
-            var activeScene = SceneManager.GetSceneByName(sceneName);
-            if (activeScene.IsValid())
-            {
-                SceneManager.SetActiveScene(activeScene);
+            if (active) {
+                var activeScene = SceneManager.GetSceneByName(sceneName);
+                if (activeScene.IsValid())
+                {
+                    SceneManager.SetActiveScene(activeScene);
+                }   
             }
         }
         
         public async UniTask UnloadScene(string sceneName)
         {
+            Logs.Log($"[SceneService]: Unloading {sceneName}...");
             var handle = SceneManager.UnloadSceneAsync(sceneName);
-            if (handle == null)
-            {
-                Logs.LogError($"[SceneService]: Encountered an error trying to unload {sceneName} !");
+            if (handle == null) {
+                Logs.LogError($"[SceneService]: Failed to unload the following scene {sceneName} !");
                 return;
             }
-
-            await handle.ToUniTask();
+            
+            await handle; 
+            Logs.Log($"[SceneService]: Successfully unloaded {sceneName}.");
         }
 
         public UniTask OnInitialize()
         {
-            _loadingGO = GameObject.Find("Loading");
+            _loadingGO = UnityEngine.GameObject.Find("Loading");
             if (_loadingGO == null)
             {
                 Logs.LogError("[SceneService]: Could not find the Loading game object in the bootstrap scene !");
