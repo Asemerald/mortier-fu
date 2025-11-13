8a8c944d10b48da0515d4ab45a133256442c7e92
Enzo Dufour <enzo.dufour05@gmail.com>
2025-11-13 18:23:35 +0100

    Update nomenclature

--- Mortier FU/Assets/00_Scripts/Runtime/Systems/EventBus/EventBinding.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Systems/EventBus/EventBinding.cs
@@ -10,26 +10,26 @@ namespace MortierFU
     }
     
     public class EventBinding<T> : IEventBinding<T> where T : IEvent {
-        Action<T> onEvent = _ => { };
-        Action onEventNoArgs = () => { };
+        Action<T> _onEvent = _ => { };
+        Action _onEventNoArgs = () => { };
 
         Action<T> IEventBinding<T>.OnEvent {
-            get => onEvent;
-            set => onEvent = value;
+            get => _onEvent;
+            set => _onEvent = value;
         }
 
         Action IEventBinding<T>.OnEventNoArgs {
-            get => onEventNoArgs;
-            set => onEventNoArgs = value;
+            get => _onEventNoArgs;
+            set => _onEventNoArgs = value;
         }
 
-        public EventBinding(Action<T> onEvent) => this.onEvent = onEvent;
-        public EventBinding(Action onEventNoArgs) => this.onEventNoArgs = onEventNoArgs;
+        public EventBinding(Action<T> onEvent) => _onEvent = onEvent;
+        public EventBinding(Action onEventNoArgs) => _onEventNoArgs = onEventNoArgs;
 
-        public void Add(Action onEvent) => onEventNoArgs += onEvent;
-        public void Remove(Action onEvent) => onEventNoArgs -= onEvent;
+        public void Add(Action onEvent) => _onEventNoArgs += onEvent;
+        public void Remove(Action onEvent) => _onEventNoArgs -= onEvent;
         
-        public void Add(Action<T> onEvent) => this.onEvent += onEvent;
-        public void Remove(Action<T> onEvent) => this.onEvent -= onEvent;
+        public void Add(Action<T> onEvent) => _onEvent += onEvent;
+        public void Remove(Action<T> onEvent) => _onEvent -= onEvent;
     }
 }
