c963f65d83cc400d417318b5229e63fb989f744d
Antoine VOLLET <a.vollet@rubika-edu.com>
2025-11-13 15:01:36 +0100

    gitflow-feature-stash: AugmentTriggers

---
+++ Mortier FU/Assets/00_Scripts/Runtime/GameSystems/Trigger/TriggerService.cs
@@ -0,0 +1,20 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace MortierFu
+{
+    public class TriggerService : IGameService
+    {
+        
+        
+        public UniTask OnInitialize()
+        {
+            return UniTask.CompletedTask;
+        }
+     
+        public void Dispose()
+        {
+        }
+        
+        public bool IsInitialized { get; set; }
+    }
+}
