36d879f9f46f042d96a57bec0fa4b45e6cec0807
Enzo Dufour <enzo.dufour05@gmail.com>
2025-11-18 17:15:20 +0100

    Création d'augment Overheating & Take the Tempo

---
+++ Mortier FU/Assets/00_Scripts/Runtime/Augments/Implementations/Offense/AGM_Overheating.cs
@@ -0,0 +1,42 @@
+﻿namespace MortierFu.Stats
+{
+    public class AGM_Overheating : AugmentBase
+    {
+        private EventBinding<TriggerShootBombshell> _shootBinding;
+        private EventBinding<TriggerEndRound> _endRoundBinding;
+        
+        public AGM_Overheating(SO_Augment augmentData, PlayerCharacter owner) : base(augmentData, owner)
+        { }
+        
+        public override void Initialize()
+        {
+            _shootBinding = new EventBinding<TriggerShootBombshell>(OnShoot);
+            EventBus<TriggerShootBombshell>.Register(_shootBinding);
+            
+            _endRoundBinding = new EventBinding<TriggerEndRound>(OnEndRound);
+            EventBus<TriggerEndRound>.Register(_endRoundBinding);
+            
+            stats.BombshellTimeTravel.AddModifier(new StatModifier(-0.9f, E_StatModType.PercentMult, this));
+        }
+        
+        private void OnShoot(TriggerShootBombshell evt)
+        {
+            if (evt.Character != owner) return;
+            
+            stats.BombshellTimeTravel.AddModifier(new StatModifier(0.05f, E_StatModType.PercentMult, this));
+        }
+        
+        private void OnEndRound(TriggerEndRound evt)
+        {
+            stats.BombshellTimeTravel.RemoveAllModifiersFromSource(this);
+            stats.BombshellTimeTravel.AddModifier(new StatModifier(-0.9f, E_StatModType.PercentMult, this));
+        }
+        
+        public override void Dispose()
+        {
+            EventBus<TriggerShootBombshell>.Deregister(_shootBinding);
+            EventBus<TriggerEndRound>.Deregister(_endRoundBinding);
+            stats.BombshellTimeTravel.RemoveAllModifiersFromSource(this);
+        }
+    }
+}
---
+++ Mortier FU/Assets/00_Scripts/Runtime/Augments/Implementations/Offense/AGM_TakeTheTempo.cs
@@ -0,0 +1,39 @@
+﻿namespace MortierFu.Stats
+{
+    public class AGM_TakeTheTempo : AugmentBase
+    {
+        private EventBinding<TriggerHit> _hitBinding;
+        private EventBinding<TriggerEndRound> _endRoundBinding;
+        
+        public AGM_TakeTheTempo(SO_Augment augmentData, PlayerCharacter owner) : base(augmentData, owner)
+        { }
+
+        public override void Initialize()
+        {
+            _hitBinding = new EventBinding<TriggerHit>(OnHit);
+            EventBus<TriggerHit>.Register(_hitBinding);
+            
+            _endRoundBinding = new EventBinding<TriggerEndRound>(OnEndRound);
+            EventBus<TriggerEndRound>.Register(_endRoundBinding);
+        }
+        
+        private void OnHit(TriggerHit evt)
+        {
+            if (evt.Bombshell.Owner != owner) return;
+            
+            stats.FireRate.AddModifier(new StatModifier(-0.2f, E_StatModType.PercentMult, this));
+        }
+        
+        private void OnEndRound(TriggerEndRound evt)
+        {
+            stats.FireRate.RemoveAllModifiersFromSource(this);
+        }
+        
+        public override void Dispose()
+        {
+            EventBus<TriggerHit>.Deregister(_hitBinding);
+            EventBus<TriggerEndRound>.Deregister(_endRoundBinding);
+            stats.FireRate.RemoveAllModifiersFromSource(this);
+        }
+    }
+}
