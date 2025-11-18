c349f55493bcb108b3e3f92eec80e639a7122524
Enzo Dufour <enzo.dufour05@gmail.com>
2025-11-18 13:19:14 +0100

    work in progress

--- Mortier FU/Assets/00_Scripts/Runtime/GameSystems/AugmentSelection/AugmentShowcaser.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/GameSystems/AugmentSelection/AugmentShowcaser.cs
@@ -1,10 +1,10 @@
 using System.Collections.ObjectModel;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using MortierFu.Shared;
 using UnityEngine;
 using PrimeTween;
 using System;
+using Cysharp.Threading.Tasks;
+using Object = UnityEngine.Object;
 using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
 
@@ -15,6 +15,9 @@ namespace MortierFu
         private readonly ReadOnlyCollection<AugmentPickup> _pickups;
         private readonly ConfirmationService _confirmationService;
         private readonly AugmentSelectionSystem _system;
+        
+        private Transform[] _augmentPoints;
+        private Tween _spinTween;
         
         private Camera _cam;
 
@@ -28,9 +31,9 @@ namespace MortierFu
             _cam = Camera.main;
         }
 
-        public async Task Showcase(List<Vector3> positions)
+        public async UniTask Showcase(Transform pivot, Vector3[] augmentPoints)
         {
-            await Task.Delay(TimeSpan.FromSeconds(_system.Settings.ShowcaseStartDelay));
+            await UniTask.Delay(TimeSpan.FromSeconds(_system.Settings.ShowcaseStartDelay));
             
             float step = _system.Settings.DisplayedCardScale * 2f + _system.Settings.CardSpacing;
             Vector3 origin = _cam.transform.position + _cam.transform.forward * 2f - _cam.transform.right * (step * (_pickups.Count - 1)) / 2f;
@@ -45,31 +48,58 @@ namespace MortierFu
                 
                 await Tween.Scale(pickup.transform, _system.Settings.DisplayedCardScale,_system.Settings.CardPopInDuration, Ease.OutBounce);
                 
-                await Task.Delay(TimeSpan.FromSeconds(0.05f));
+                await UniTask.Delay(TimeSpan.FromSeconds(0.05f));
             }
             
-            await Task.Delay(TimeSpan.FromSeconds(_system.Settings.CardPopInDuration));
+            await UniTask.Delay(TimeSpan.FromSeconds(_system.Settings.CardPopInDuration));
             await _confirmationService.WaitUntilAllConfirmed();
 
-            if (_pickups.Count != positions.Count)
+            if (_pickups.Count != augmentPoints.Length)
             {
                 Logs.LogWarning("[AugmentShowcaser] Number of pickups and positions do not match.");
                 return;
             }
             
+            _augmentPoints = new Transform[_pickups.Count];
+            
             for (var i = 0; i < _pickups.Count; i++)
             {
                 var pickup = _pickups[i];
+                
+                var augmentPoint = new GameObject("Augment Point #" + i).transform;
+                augmentPoint.SetParent(pivot);
+                _augmentPoints[i] = augmentPoint;
+                
+                pickup.transform.SetParent(_augmentPoints[i]);
+                
                 var duration = _system.Settings.CardMoveDurationRange.GetRandomValue();
-                await Tween.Position(pickup.transform, positions[i].Add(y: i * 0.06f), duration, Ease.InOutQuad)
+                Tween.Position(pickup.transform, _augmentPoints[i].position.Add(y: i * 0.06f), duration, Ease.InOutQuad)
                     .Group(Tween.Scale(pickup.transform, _system.Settings.DisplayedCardScale, 1f, duration * 0.7f, Ease.OutBack)).OnComplete(() =>
                     {
                         pickup.SetFaceCameraEnabled(false);
                         pickup.transform.rotation = Quaternion.Euler(90f, 0f, 0f);
                     });
 
-                await Task.Delay(TimeSpan.FromSeconds(_system.Settings.CardMoveStaggerRange.GetRandomValue()));
+                await UniTask.Delay(TimeSpan.FromSeconds(_system.Settings.CardMoveStaggerRange.GetRandomValue()));
             }
+            
+            _spinTween = Tween.RotationAtSpeed(pivot, Vector3.up * 360f, 2f, Ease.Linear, -1, CycleMode.Incremental);
+        }
+        
+        public void StopShowcase()
+        {
+            foreach (var pickup in _pickups)
+            {
+                pickup.transform.SetParent(null);
+            }
+
+            for (int i = _augmentPoints.Length - 1; i >= 0; i--)
+            {
+                Object.Destroy(_augmentPoints[i].gameObject);
+            }
+            
+            _spinTween.Stop();
+            _spinTween = default;
         }
 
         public void Dispose()
