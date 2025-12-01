f3b0374e100c7ef5b3881361edb258bce0b70191
Enzo Dufour <enzo.dufour05@gmail.com>
2025-12-01 10:56:50 +0100

    Rangement

--- Mortier FU/Assets/00_Scripts/Runtime/Actors/Breakable.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Actors/Breakable.cs
@@ -1,11 +1,10 @@
-using MortierFu.Shared;
 using UnityEngine;
 
 namespace MortierFu
 {
     public class Breakable : MonoBehaviour, IInteractable
     {
-        [SerializeField] private int life = 1;
+        [SerializeField] private int _life = 1;
         [SerializeField] private Material _mat;
     
         private MeshRenderer _meshRenderer;
@@ -17,8 +16,8 @@ namespace MortierFu
 
         public void Interact()
         {
-            life--;
-            if (life <= 0)
+            _life--;
+            if (_life <= 0)
             { 
                 Destroy(gameObject); 
                 return;
--- Mortier FU/Assets/00_Scripts/Runtime/Actors/VehiculeDamage.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Actors/VehiculeDamage.cs
@@ -1,4 +1,3 @@
-using System;
 using MortierFu;
 using UnityEngine;
 
