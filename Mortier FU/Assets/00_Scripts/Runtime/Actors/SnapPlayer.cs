b9a5cc4a6bebaed7dd6a693fb399b274efb06e17
Enzo Dufour <enzo.dufour05@gmail.com>
2025-12-02 11:05:27 +0100

    Correction d'un bug

--- Mortier FU/Assets/00_Scripts/Runtime/Actors/SnapPlayer.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Actors/SnapPlayer.cs
@@ -5,20 +5,17 @@ namespace MortierFu
     public class SnapPlayer : MonoBehaviour
     {
         private void OnTriggerEnter(Collider other)
-        {   Debug.Log("1");
+        {
             if (other.TryGetComponent(out PlayerCharacter character))
             {
-                Debug.Log("snaped");
                 character.gameObject.transform.SetParent(gameObject.transform.parent.gameObject.transform);
             }
         }
     
         private void OnTriggerExit(Collider other)
         {
-            Debug.Log("2");
             if (other.TryGetComponent(out PlayerCharacter character))
             {
-                Debug.Log("Unsnaped");
                 character.gameObject.transform.SetParent(null);
             }
         }
