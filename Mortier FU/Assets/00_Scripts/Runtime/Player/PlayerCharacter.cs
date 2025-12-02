167c99cf725f90b6dae0680ccf5b47a9603f2c3a
Pediluv <s.sindt@rubika-edu.com>
2025-12-02 15:54:16 +0100

    Script : AimIndicator added

--- Mortier FU/Assets/00_Scripts/Runtime/Player/PlayerCharacter.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Player/PlayerCharacter.cs
@@ -6,6 +6,7 @@ using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace MortierFu
 {
@@ -111,6 +112,8 @@ namespace MortierFu
             Controller.Initialize();
             Aspect.Initialize(); // Require to be initialized before the mortar
             Mortar.Initialize();
+            //TEMP Initialiser l'aimindicator
+            GetComponent<TEMP_AimIndicatorSystem>().Initialize();
 
             _toggleAimAction.started += Mortar.BeginAiming;
             _toggleAimAction.canceled += Mortar.EndAiming;
