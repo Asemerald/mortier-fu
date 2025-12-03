5834601bce0cecba30a3f5a583a2749c5be5c32f
Enzo Dufour <enzo.dufour05@gmail.com>
2025-12-03 18:42:54 +0100

    Side quest du puddle

---
+++ Mortier FU/Assets/00_Scripts/Runtime/Augments/AGM_PoisonPuddle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace MortierFu
+{
+    [Serializable]
+    public class AGM_PoisonPuddle : ElementAugmentBase
+    {
+        [Serializable]
+        public struct Params
+        {
+            public GameObject PuddlePrefab;
+            
+            public AugmentStatMod PoisonDamageMod;
+            public AugmentStatMod Duration;
+            public AugmentStatMod TickInterval;
+        }
+
+        public AGM_PoisonPuddle(SO_Augment augmentData, PlayerCharacter owner, SO_AugmentDatabase db) : base(
+            augmentData, owner, db)
+        { }
+
+        protected override void OnTriggerBombshellImpact(TriggerBombshellImpact evt)
+        {
+            if (evt.Bombshell.Owner != Owner) return;
+
+            var puddleObj = GameObject.Instantiate(
+                db.PoisonPuddleParams.PuddlePrefab,
+                evt.Bombshell.transform.position + Vector3.up,
+                Quaternion.identity
+            );
+            
+            var puddle = puddleObj.GetComponent<PuddleController>();
+            
+            puddle.Lifetime = db.PoisonPuddleParams.Duration.Value;
+        }
+
+        protected override void OnTriggerEndRound(TriggerEndRound evt)
+        {
+        }
+    }
+}
---
+++ Mortier FU/Assets/00_Scripts/Runtime/Augments/PuddleController.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MortierFu
+{
+    public class PuddleController : MonoBehaviour
+    {
+        public List<Ability> Abilities;
+        public float Lifetime = 5f;
+
+        private HashSet<PlayerCharacter> _inside = new();
+
+        private void Start()
+        {
+            Destroy(gameObject, Lifetime);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var rb = other.attachedRigidbody;
+            if (rb != null && rb.TryGetComponent(out PlayerCharacter character))
+            {
+                _inside.Add(character);
+                ApplyEffects(character);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            var rb = other.attachedRigidbody;
+            if (rb != null && rb.TryGetComponent(out PlayerCharacter character))
+            {
+                CancelEffects(character);
+                _inside.Remove(character);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            foreach (var player in _inside)
+                CancelEffects(player);
+        }
+
+        private void ApplyEffects(PlayerCharacter target)
+        {
+            foreach (var effect in Abilities)
+            {
+                // TODO: Get real owner
+                effect.Execute(target,target);
+            }
+        }
+
+        private void CancelEffects(PlayerCharacter target)
+        {
+        }
+    }
+}
