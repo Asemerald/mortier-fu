e3ce4acf21dc197e962415e858edfd38b3e55c06
Enzo Dufour <enzo.dufour05@gmail.com>
2025-12-03 18:42:54 +0100

    Side quest du puddle

--- Mortier FU/Assets/00_Scripts/Runtime/Augments/SO_AugmentDatabase.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Augments/SO_AugmentDatabase.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace MortierFu
 {
@@ -22,6 +22,7 @@ namespace MortierFu
         public AGM_SharperBullets.Params SharperBulletsParams;
         public AGM_TakeTheTempo.Params TakeTheTempoParams;
         public AGM_TsarBomba.Params TsarBombaParams;
+        public AGM_PoisonPuddle.Params PoisonPuddleParams;
         
         [Header("Strike")]
         public AGM_BigStrike.Params BigStrikeParams;
