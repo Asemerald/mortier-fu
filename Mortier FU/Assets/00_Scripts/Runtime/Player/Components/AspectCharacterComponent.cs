292075e985812c07b474271111dc2be23830627c
Padrox <a.vollet@rubika-edu.com>
2025-12-16 21:17:55 +0100

    Pouf

--- Mortier FU/Assets/00_Scripts/Runtime/Player/Components/AspectCharacterComponent.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Player/Components/AspectCharacterComponent.cs
@@ -10,22 +10,17 @@ namespace MortierFu {
         public Material TrailThinMat;
         public Material TrailFatMat;
         public ParticleSystem.MinMaxGradient LightColor;
+        [ColorUsage(false)] public Color PlayerColor;
     }
     
-    public class AspectCharacterComponent : CharacterComponent
+    public class AspectCharacterComponent : CharacterComponent 
     {
-        public Color PlayerColor { get; private set; }
-        private float _hueOffset;
-        private float _saturation;
-        private float _value;
+        public Color PlayerColor => AspectMaterials.PlayerColor;
         
         public CharacterAspectMaterials AspectMaterials { get; private set; }
         
-        public AspectCharacterComponent(PlayerCharacter character, float hueOffset, float saturation, float value) : base(character) {
-            _hueOffset = hueOffset;
-            _saturation = saturation;
-            _value = value;
-        }
+        public AspectCharacterComponent(PlayerCharacter character) : base(character) 
+        { }
 
         public void SetAspectMaterials(CharacterAspectMaterials aspectMaterials)
         {
@@ -37,9 +32,8 @@ namespace MortierFu {
             int playerCount = lobbyService.GetPlayers().Count;
             
             // Retrieve this player's index
-            int index = character.Owner.PlayerIndex;
+            //int index = character.Owner.PlayerIndex;
             //PlayerColor = GetColorForPlayerIndex(index, playerCount, _hueOffset, _saturation, _value);\
-            PlayerColor = AspectMaterials.LightColor.color;
             
             var renderers = character.GetComponentsInChildren<SkinnedMeshRenderer>();
             if (renderers.Length > 0)
@@ -57,13 +51,12 @@ namespace MortierFu {
         public override void Dispose()
         { }
 
-        private static Color GetColorForPlayerIndex(int index, int totalPlayers,
-            float hueOffset = 0f, float saturation = 1f, float value = 1f)
-        {
-            float segment = 1f / Mathf.Max(1, totalPlayers);
-            float hue = (segment * index + hueOffset) % 1f;
-            return Color.HSVToRGB(hue, saturation, value);
-        }
+        // private static Color GetColorForPlayerIndex(int index, int totalPlayers,
+        //     float hueOffset = 0f, float saturation = 1f, float value = 1f)
+        // {
+        //     float segment = 1f / Mathf.Max(1, totalPlayers);
+        //     float hue = (segment * index + hueOffset) % 1f;
+        //     return Color.HSVToRGB(hue, saturation, value);
+        // }
     }
-   
 }
