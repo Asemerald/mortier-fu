7cd3c923b4f721b08bd454f50e07e61efc4113b0
Ase <tdruesne05@gmail.com>
2025-12-18 15:13:38 +0100

    AutoBuild Addressables + Git Version added

---
+++ Mortier FU/Assets/00_Scripts/Editor/BuildScripts/AutoAddressablesBuild.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEditor.AddressableAssets.Settings;
+
+namespace MortierFu.Editor
+{
+    public class AutoAddressablesBuild
+    {
+        /// <summary>
+        /// Run a clean build before export.
+        /// </summary>
+        static public void PreExport()
+        {
+            Debug.Log("BuildAddressablesProcessor.PreExport start");
+            AddressableAssetSettings.CleanPlayerContent();
+            AddressableAssetSettings.BuildPlayerContent();
+            Debug.Log("BuildAddressablesProcessor.PreExport done");
+        }
+
+        [InitializeOnLoadMethod]
+        private static void Initialize()
+        {
+            BuildPlayerWindow.RegisterBuildPlayerHandler(BuildPlayerHandler);
+        }
+
+        private static void BuildPlayerHandler(BuildPlayerOptions options)
+        {
+            PreExport();
+            BuildPlayerWindow.DefaultBuildMethods.BuildPlayer(options);
+        }
+    }
+
+}
---
+++ Mortier FU/Assets/00_Scripts/Editor/BuildScripts/AutoGitVersion.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace MortierFu.Editor
+{
+    public class AutoGitVersion : IPreprocessBuildWithReport
+    {
+        // Priorité d'exécution 
+        public int callbackOrder => 0;
+
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            string commit = GetGitCommit();
+            string branch = GetGitBranch();
+
+            // [branch]-[commit]
+            PlayerSettings.bundleVersion = $"{branch}-{commit}";
+            UnityEngine.Debug.Log($"[AutoGitVersion] Version set to: {PlayerSettings.bundleVersion}");
+        }
+
+        private string GetGitCommit()
+        {
+            return RunGitCommand("rev-parse --short HEAD");
+        }
+
+        private string GetGitBranch()
+        {
+            return RunGitCommand("rev-parse --abbrev-ref HEAD");
+        }
+
+        private string RunGitCommand(string args)
+        {
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "git";
+                process.StartInfo.Arguments = args;
+                process.StartInfo.WorkingDirectory = Application.dataPath;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+
+                string output = process.StandardOutput.ReadLine();
+                process.WaitForExit();
+                return string.IsNullOrEmpty(output) ? "N/A" : output.Trim();
+            }
+            catch
+            {
+                return "N/A";
+            }
+        }
+    }
+}
