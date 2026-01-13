8213a08a0ad65fd5f7500c2c3a2991ba43585a18
Enzo Dufour <enzo.dufour05@gmail.com>
2026-01-13 17:45:56 +0100

    Start pause

--- Mortier FU/Assets/00_Scripts/Runtime/GameSystems/PauseSystem.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/GameSystems/PauseSystem.cs
@@ -1,14 +1,89 @@
 using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace MortierFu
 {
     public class PauseSystem : IGameSystem
     {
+        private SaveService _saveService;
+
+        public void RestoreSettingsFromSave()
+        {
+            var s = _saveService.Settings;
+            Screen.fullScreen = s.IsFullscreen;
+            QualitySettings.vSyncCount = s.IsVSyncEnabled ? 1 : 0;
+
+            // TODO : Apply volume settings to FMOD Bus
+        }
+
+        public void UpdateUIFromSave(Toggle fullscreenToggle, Toggle vsyncToggle, Slider masterVolumeSlider,
+            Slider musicVolumeSlider, Slider sFXVolumeSlider)
+        {
+            var s = _saveService.Settings;
+
+            fullscreenToggle.isOn = s.IsFullscreen;
+            vsyncToggle.isOn = s.IsVSyncEnabled;
+
+            masterVolumeSlider.value = s.MasterVolume;
+            musicVolumeSlider.value = s.MusicVolume;
+            sFXVolumeSlider.value = s.SfxVolume;
+        }
+
+        public void BindUIEvents(Toggle fullscreenToggle, Toggle vsyncToggle, Slider MasterVolumeSlider,
+            Slider MusicVolumeSlider, Slider SFXVolumeSlider)
+        {
+            fullscreenToggle.onValueChanged.AddListener(OnFullscreenChanged);
+            vsyncToggle.onValueChanged.AddListener(OnVSyncChanged);
+
+            MasterVolumeSlider.onValueChanged.AddListener(OnMasterVolumeChanged);
+            MusicVolumeSlider.onValueChanged.AddListener(OnMusicVolumeChanged);
+            SFXVolumeSlider.onValueChanged.AddListener(OnSfxVolumeChanged);
+        }
+
+        private void OnFullscreenChanged(bool value)
+        {
+            Screen.fullScreen = value;
+            _saveService.Settings.IsFullscreen = value;
+        }
+
+        private void OnVSyncChanged(bool value)
+        {
+            QualitySettings.vSyncCount = value ? 1 : 0;
+            _saveService.Settings.IsVSyncEnabled = value;
+        }
+
+        private void OnMasterVolumeChanged(float value)
+        {
+            // TODO : Apply volume to FMOD Bus
+            _saveService.Settings.MasterVolume = value;
+        }
+
+        private void OnMusicVolumeChanged(float value)
+        {
+            // TODO : Apply volume to FMOD Bus
+            _saveService.Settings.MusicVolume = value;
+        }
+
+        private void OnSfxVolumeChanged(float value)
+        {
+            // TODO : Apply volume to FMOD Bus
+            _saveService.Settings.SfxVolume = value;
+        }
+
+        public void SaveSettings()
+        {
+            _saveService.SaveSettings().Forget();
+        }
+        
         public void Dispose()
-        { }
+        {
+        }
 
         public UniTask OnInitialize()
         {
+            _saveService = ServiceManager.Instance.Get<SaveService>();
+
             return UniTask.CompletedTask;
         }
 
--- Mortier FU/Assets/00_Scripts/Runtime/UI/InGame/PauseUI.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/UI/InGame/PauseUI.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PauseUI : MonoBehaviour
+namespace MortierFu
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class PauseUI : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Toggle _fullscreenToggle;
 
-    // Update is called once per frame
-    void Update()
-    {
+        [SerializeField] private Toggle _vSyncToggle;
+
+        [SerializeField] private Slider _masterVolumeSlider;
+        [SerializeField] private Slider _musicVolumeSlider;
+        [SerializeField] private Slider _sfxVolumeSlider;
+
+        private PauseSystem _pauseSystem;
+
+        private void Awake()
+        {
+            Hide();
+            
+            _pauseSystem = SystemManager.Instance.Get<PauseSystem>();
+            _pauseSystem.RestoreSettingsFromSave();
+            _pauseSystem.UpdateUIFromSave(_fullscreenToggle, _vSyncToggle, _masterVolumeSlider, _musicVolumeSlider,
+                _sfxVolumeSlider);
+            _pauseSystem.BindUIEvents(_fullscreenToggle, _vSyncToggle, _masterVolumeSlider, _musicVolumeSlider,
+                _sfxVolumeSlider);
+        }
+
+        private void Start()
+        {
+            _pauseSystem.SaveSettings();
+        }
+
+        private void Resume()
+        {
+            Hide();
+        }
+
+        private void PauseGame()
+        {
+            Show();
+        }
+
+        private void Hide() => gameObject.SetActive(false);
+        private void Show() => gameObject.SetActive(true);
         
     }
 }
