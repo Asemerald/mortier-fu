6292cf8c4475bd49cd3b7e32b78711f8df80e4b3
Enzo Dufour <enzo.dufour05@gmail.com>
2026-01-15 22:56:55 +0100

    Réglage slider

---
+++ Mortier FU/Assets/00_Scripts/Runtime/UI/SliderController.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+namespace MortierFu
+{
+    public class SliderController : MonoBehaviour
+    {
+        [SerializeField] private Slider _fakeSlider;
+        [SerializeField] private Slider _sliderToControl;
+
+        private bool _isEditing;
+
+        private Navigation _cachedNavigation;
+
+        private InputAction _submitAction;
+
+        private LobbyService _lobbyService;
+
+        private void Awake()
+        {
+            _cachedNavigation = _sliderToControl.navigation;
+            _sliderToControl.interactable = false;
+        }
+
+        private void Start()
+        {
+            _lobbyService = ServiceManager.Instance.Get<LobbyService>();
+
+            _submitAction = _lobbyService.Players[0].PlayerInput.actions.FindAction("Submit");
+            _submitAction.started += SetNavigation;
+        }
+
+        private void OnDestroy()
+        {
+            _submitAction.started -= SetNavigation;
+        }
+
+        private void SetNavigation(InputAction.CallbackContext context)
+        {
+            if (EventSystem.current.currentSelectedGameObject != _fakeSlider.gameObject && 
+                EventSystem.current.currentSelectedGameObject != _sliderToControl.gameObject)
+                return;
+
+            _isEditing = !_isEditing;
+
+            if (_isEditing)
+            {
+                EnterEditMode();
+            }
+            else
+            {
+                ExitEditMode();
+            }
+        }
+
+        private void EnterEditMode()
+        {
+            _sliderToControl.interactable = true;
+
+            var nav = _sliderToControl.navigation;
+            nav.mode = Navigation.Mode.None;
+            _sliderToControl.navigation = nav;
+
+            EventSystem.current.SetSelectedGameObject(_sliderToControl.gameObject);
+        }
+
+        private void ExitEditMode()
+        {
+            _sliderToControl.interactable = false;
+            _sliderToControl.navigation = _cachedNavigation;
+
+            EventSystem.current.SetSelectedGameObject(_fakeSlider.gameObject);
+        }
+    }
+}
