c08a3df853d0a296e212db6dd1b9d59dd3448b4f
Explosia181 <123968825+Explosia181@users.noreply.github.com>
2026-01-15 16:27:43 +0100

    Import BoxDestroy

---
+++ Mortier FU/Assets/00_Scripts/GA/Breakable.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace MortierFu
+{
+    public class Breakable : MonoBehaviour
+    {
+        [SerializeField] GameObject SM_Box
+        [SerializeField] GameObject SM_BoxDestroy
+        
+            
+
+    
+    }
+}
