38498fbdfc66168c1523994418ca01aeec2ef7ff
Explosia181 <123968825+Explosia181@users.noreply.github.com>
2026-01-15 16:29:13 +0100

    Add a ScriptTest Breakable into GA Script folder

--- Mortier FU/Assets/00_Scripts/GA/Breakable.cs
+++ Mortier FU/Assets/00_Scripts/GA/Breakable.cs
@@ -1,14 +1,36 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace MortierFu
 {
     public class Breakable : MonoBehaviour
     {
-        [SerializeField] GameObject SM_Box
-        [SerializeField] GameObject SM_BoxDestroy
-        
-            
+        [SerializeField] GameObject SM_Box:
+        [SerializeField] GameObject SM_BoxDestroy;
 
-    
+        BoxCollider bc;
+
+        private void Awake()
+        {
+            SM_Box.SetActive(true);
+            SM_BoxDestroy.SetActive(false);
+
+            bc = GetComponent<BoxCollider>();
+
+        }
+
+        private void OnMouseDown()
+        {
+            Break();
+        }
+
+       
+        {
+            )
+        }
+
+
+
+
     }
 }
