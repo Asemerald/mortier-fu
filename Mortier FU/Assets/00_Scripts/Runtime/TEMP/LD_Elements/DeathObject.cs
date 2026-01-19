ecbcc126d81d545987b2a0fe7f2527236f7aef65
Enzo Dufour <enzo.dufour05@gmail.com>
2026-01-19 20:13:34 +0100

    Haptique death object

--- Mortier FU/Assets/00_Scripts/Runtime/TEMP/LD_Elements/DeathObject.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/TEMP/LD_Elements/DeathObject.cs
@@ -3,12 +3,19 @@ using MortierFu;
 
 public class DeathObject : MonoBehaviour
 {
+    private ShakeService _shakeService;
+    private void Start()
+    {
+        _shakeService = ServiceManager.Instance.Get<ShakeService>();
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.rigidbody != null && other.rigidbody.TryGetComponent(out PlayerCharacter character))
         {
             character.Health.TakeLethalDamage(gameObject);
             AudioService.PlayOneShot(AudioService.FMODEvents.SFX_Player_Fall, other.transform.position);
+            _shakeService.ShakeController(character.Owner, ShakeService.ShakeType.MID);
         }
     }
 }
