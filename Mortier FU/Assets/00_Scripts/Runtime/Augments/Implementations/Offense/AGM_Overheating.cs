337599d684e154bfe44b9419e857ab7e44a1c9ea
MartiinThullier <m.thullier@rubika-edu.com>
2026-01-20 19:53:26 +0100

    overheating ou quoi

--- Mortier FU/Assets/00_Scripts/Runtime/Augments/Implementations/Offense/AGM_Overheating.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Augments/Implementations/Offense/AGM_Overheating.cs
@@ -5,17 +5,13 @@
         [System.Serializable]
         public struct Params
         {
-            public AugmentStatMod BombshellSpeedMod;
-            public AugmentStatMod OnHitBombshellSpeedMod;
-            
-            public AugmentStatMod FireRateMod;
-            public AugmentStatMod OnHitFireRateMod;
-            
-            public AugmentStatMod OnHitMoveSpeedMod;
+            public AugmentStatMod OnAttackBombshellSpeedMod;
         }
         
         private EventBinding<TriggerShootBombshell> _shootBinding;
         private EventBinding<TriggerEndRound> _endRoundBinding;
+        
+        private int StackAmount;
         
         public AGM_Overheating(SO_Augment augmentData, PlayerCharacter owner,  SO_AugmentDatabase db) : base(augmentData, owner, db)
         { }
@@ -27,27 +23,26 @@
             
             _endRoundBinding = new EventBinding<TriggerEndRound>(OnEndRound);
             EventBus<TriggerEndRound>.Register(_endRoundBinding);
-            
-            stats.BombshellSpeed.AddModifier(db.OverheatingParams.BombshellSpeedMod.ToMod(this));
-            stats.FireRate.AddModifier(db.OverheatingParams.FireRateMod.ToMod(this));
+
+            StackAmount = 0;
         }
         
         private void OnShoot(TriggerShootBombshell evt)
         {
             if (evt.Character != owner) return;
+
+            if (StackAmount <= 10)
+            {
+                stats.BombshellSpeed.AddModifier(db.OverheatingParams.OnAttackBombshellSpeedMod.ToMod(this));
+                StackAmount++;
+            }
             
-            stats.BombshellSpeed.AddModifier(db.OverheatingParams.OnHitBombshellSpeedMod.ToMod(this));
-            stats.FireRate.AddModifier(db.OverheatingParams.OnHitFireRateMod.ToMod(this));
-            stats.MoveSpeed.AddModifier(db.OverheatingParams.OnHitMoveSpeedMod.ToMod(this));
         }
         
         private void OnEndRound(TriggerEndRound evt)
         {
             stats.BombshellSpeed.RemoveAllModifiersFromSource(this);
-            stats.FireRate.RemoveAllModifiersFromSource(this);
-            stats.MoveSpeed.RemoveAllModifiersFromSource(this);
-            stats.BombshellSpeed.AddModifier(db.OverheatingParams.BombshellSpeedMod.ToMod(this));
-            stats.FireRate.AddModifier(db.OverheatingParams.FireRateMod.ToMod(this));
+            StackAmount = 10;
         }
         
         public override void Dispose()
@@ -55,8 +50,6 @@
             EventBus<TriggerShootBombshell>.Deregister(_shootBinding);
             EventBus<TriggerEndRound>.Deregister(_endRoundBinding);
             stats.BombshellSpeed.RemoveAllModifiersFromSource(this);
-            stats.FireRate.RemoveAllModifiersFromSource(this);
-            stats.MoveSpeed.RemoveAllModifiersFromSource(this);
         }
     }
 }
