c078684803b7681b47cd32cbb973a769c046e222
THULLIER Martin <m.thullier@rubika-edu.com>
2026-01-22 12:20:47 +0100

    setup inté son overheating & take the tempo

--- Mortier FU/Assets/00_Scripts/Runtime/Augments/Implementations/Offense/AGM_TakeTheTempo.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Augments/Implementations/Offense/AGM_TakeTheTempo.cs
@@ -6,10 +6,13 @@
         public struct Params
         {
             public AugmentStatMod FireRateMod;
+            public float MaxStacksTempo;
         }
         
         private EventBinding<TriggerHit> _hitBinding;
         private EventBinding<TriggerEndRound> _endRoundBinding;
+
+        private int stackAmountTempo;
         
         public AGM_TakeTheTempo(SO_Augment augmentData, PlayerCharacter owner,  SO_AugmentDatabase db) : base(augmentData, owner, db)
         { }
@@ -21,19 +24,28 @@
             
             _endRoundBinding = new EventBinding<TriggerEndRound>(OnEndRound);
             EventBus<TriggerEndRound>.Register(_endRoundBinding);
+            
+            stackAmountTempo = 0;
         }
         
         private void OnHit(TriggerHit evt)
         {
             if (evt.Bombshell.Owner != owner) return;
             
-            stats.FireRate.AddModifier(db.TakeTheTempoParams.FireRateMod.ToMod(this));
-            AudioService.PlayOneShot(AudioService.FMODEvents.SFX_Augment_Buff, owner.transform.position);
+            if (stackAmountTempo <= db.TakeTheTempoParams.MaxStacksTempo)
+            {
+                stats.FireRate.AddModifier(db.TakeTheTempoParams.FireRateMod.ToMod(this));
+                AudioService.PlayOneShot(AudioService.FMODEvents.SFX_Augment_Buff, owner.transform.position);
+                stackAmountTempo++;
+            }
+            
+            
         }
         
         private void OnEndRound(TriggerEndRound evt)
         {
             stats.FireRate.RemoveAllModifiersFromSource(this);
+            stackAmountTempo = 0;
         }
         
         public override void Dispose()
