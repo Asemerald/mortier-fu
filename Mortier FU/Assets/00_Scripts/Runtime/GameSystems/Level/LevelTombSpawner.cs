258c6042c391c4cf9350a567361eaee407bcb522
Antoine VOLLET <a.vollet@rubika-edu.com>
2026-01-23 14:56:37 +0100

    Water tomb progress

--- Mortier FU/Assets/00_Scripts/Runtime/GameSystems/Level/LevelTombSpawner.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/GameSystems/Level/LevelTombSpawner.cs
@@ -1,8 +1,15 @@
-﻿using UnityEngine;
+﻿using MortierFu.Shared;
+using PrimeTween;
+using UnityEngine;
 
 namespace MortierFu {
     public class LevelTombSpawner : MonoBehaviour {
         [SerializeField] private GameObject[] _tombPrefabs;
+        [SerializeField] private GameObject[] _waterTombPrefabs;
+        [SerializeField] private float _waterTombOffsetY = 1.5f;
+        [SerializeField] private float _waterRiseDuration = 1.3f;
+        [SerializeField] private float _waterRaycastHeight = 10f;
+        [SerializeField] private Ease _waterRiseEase = Ease.OutCubic;
         private EventBinding<EventPlayerDeath> _playerDeathBinding;
         
         private void OnEnable() {
@@ -33,7 +40,24 @@ namespace MortierFu {
                 }
                 case E_DeathCause.Fall:
                 {
-                    // Spawn water prefab
+                    var prefab = _waterTombPrefabs[index];
+
+                    Vector3 characterPos = evt.Character.transform.position;
+                    
+                    if (Physics.Raycast(characterPos.Add(y: _waterRaycastHeight), Vector3.down, out RaycastHit hit,
+                                         _waterRaycastHeight * 1.5f, LayerMask.NameToLayer("Water"), 
+                                         QueryTriggerInteraction.Collide))
+                    {
+                        var tomb = Instantiate(prefab, hit.point.Add(-_waterTombOffsetY), Quaternion.identity, new InstantiateParameters()
+                        {
+                            scene = gameObject.scene
+                        });
+
+                        Tween.Position(tomb.transform, characterPos, _waterRiseDuration, _waterRiseEase);
+                    }
+                    
+                    Debug.DrawLine(characterPos.Add(y:  _waterRaycastHeight), characterPos.Add(y: _waterRaycastHeight * 1.5f), Color.red, 3f);
+                    
                     break;
                 }
                 case E_DeathCause.VehicleCrash:
