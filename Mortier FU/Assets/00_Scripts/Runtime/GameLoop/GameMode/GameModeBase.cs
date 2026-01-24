1ff08b337554ce86bbbf6106a0e515394a020e2a
Enzo Dufour <enzo.dufour05@gmail.com>
2026-01-24 17:06:02 +0100

    Spawn player and dont show

--- Mortier FU/Assets/00_Scripts/Runtime/GameLoop/GameMode/GameModeBase.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/GameLoop/GameMode/GameModeBase.cs
@@ -237,7 +237,7 @@ namespace MortierFu
             }
         }
 
-        private void SpawnPlayers(bool useRoundWinnerSpawnPoint = false)
+        private void SpawnPlayers()
         {
             bool opposite = _currentRound.RoundIndex % 2 == 0;
             int spawnIndex = opposite ? teams.Sum(t => t.Members.Count()) - 1 : 0;
@@ -256,23 +256,26 @@ namespace MortierFu
                     }
                     else
                     {
-                        if (useRoundWinnerSpawnPoint && team.Rank == 1)
-                        {
-                            spawnPoint = levelSystem.GetRoundWinnerSpawnPoint();
-                        }
-                        else
-                        {
-                            spawnPoint = levelSystem.GetSpawnPoint(spawnIndex);
-                        }
+                        spawnPoint = levelSystem.GetSpawnPoint(spawnIndex);
                     }
 
-                    member.SpawnInGame(spawnPoint.position, spawnPoint.rotation);                    
+                    member.SpawnInGame(spawnPoint.position, spawnPoint.rotation);
 
                     if (opposite)
                         spawnIndex--;
                     else
                         spawnIndex++;
                 }
+            }
+        }
+
+        private void SpawnWinnerTeam(PlayerTeam winnerTeam)
+        {
+            var spawnPoint = levelSystem.GetRoundWinnerSpawnPoint();
+
+            foreach (var member in winnerTeam.Members)
+            {
+                member.SpawnInGame(spawnPoint.position, spawnPoint.rotation);
             }
         }
 
@@ -394,8 +397,6 @@ namespace MortierFu
                 stormInstance.Stop();
             }
 
-            ResetPlayers();
-            SpawnPlayers(true);
             EventBus<TriggerEndRound>.Raise(new TriggerEndRound());
             EnablePlayerInputs(false);
             PlayerCharacter.AllowGameplayActions = false;
@@ -405,13 +406,17 @@ namespace MortierFu
 
             _currentRound.WinningTeam = teams.FirstOrDefault(t => t.Rank == 1);
 
-
             if (_currentRound.WinningTeam != null)
             {
+                var winner = _currentRound.WinningTeam.Members[0];
+
+                winner.Character.Reset();
+                SpawnWinnerTeam(_currentRound.WinningTeam);
+
                 cameraSystem.Controller.EndFightCameraMovement(
-                    _currentRound.WinningTeam.Members[0].Character.transform);
-                _currentRound.WinningTeam.Members[0].transform.rotation = levelSystem.GetRoundWinnerSpawnPoint().rotation;
-                _currentRound.WinningTeam.Members[0].Character.WinRoundDance();
+                    winner.Character.transform);
+
+                winner.Character.WinRoundDance();
             }
 
             OnRoundEnded?.Invoke(_currentRound);
@@ -538,7 +543,6 @@ namespace MortierFu
 
         public virtual void EndGame()
         {
-            
             ServiceManager.Instance.Get<AudioService>().StartMusic(AudioService.FMODEvents.MUS_Victory).Forget();
             OnGameEnded?.Invoke(GetWinnerPlayerIndex());
             Logs.Log("Game has ended.");
@@ -558,7 +562,7 @@ namespace MortierFu
             currentState = newState;
             OnGameStateChanged?.Invoke(newState);
         }
-        
+
         public void SetScoreToWin(int score)
         {
             ScoreToWin = score;
