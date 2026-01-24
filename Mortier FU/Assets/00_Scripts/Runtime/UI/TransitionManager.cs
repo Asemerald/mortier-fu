eba566071b834d3c4775c9871519f31ea793366c
Ase <tdruesne05@gmail.com>
2026-01-24 12:40:20 +0100

    update rider package

--- Mortier FU/Assets/00_Scripts/Runtime/UI/TransitionManager.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/UI/TransitionManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using MortierFu.Shared;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -9,11 +10,24 @@ namespace MortierFu
 {
     public class TransitionManager : MonoBehaviour
     {
+        public static TransitionManager Instance { get; private set; }
+        
         private static readonly int MaskAmount = Shader.PropertyToID("_MaskAmount");
         [SerializeField] private Material material;
 
         private float maskAmount = 0f;
         private float targetValue = 1f;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Logs.LogWarning("[TransitionManager]: Multiple instances detected. Destroying duplicate.", this);
+                Destroy(this.gameObject);
+                return;
+            }
+            Instance = this;
+        }
 
         private void Start()
         {
@@ -21,18 +35,28 @@ namespace MortierFu
         }
 
         private void Update() {
-            if (Keyboard.current.tKey.wasPressedThisFrame) {
-                targetValue = -.1f;
-            }
-            if (Keyboard.current.yKey.wasPressedThisFrame) {
-                targetValue = 1f;
-            }
-
             float maskAmountChange = targetValue > maskAmount ? +.1f : -.1f;
             maskAmount += maskAmountChange * Time.deltaTime * 6f; //TODO: expose speed
             maskAmount = Mathf.Clamp01(maskAmount);
 
             material.SetFloat(MaskAmount, maskAmount);
         }
+        
+        public void FadeIn()
+        {
+            targetValue = 1f;
+        }
+        
+        public void EndTransition()
+        {
+            targetValue = 0f;
+        }
+        
+        public bool IsTransitionning()
+        {
+            return !Mathf.Approximately(maskAmount, targetValue);
+        }
+        
+        
     }
 }
