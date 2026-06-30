1dc400c792df3bd78c1cd133a93ded8a34014528
Enzo Dufour <enzo.dufour05@gmail.com>
2026-06-30 17:04:50 +0200

    [UPDATE] Modification du GameInitializer et prefab

--- Mortier FU/Assets/00_Scripts/Runtime/Core/Initialization/GameInitializer.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Core/Initialization/GameInitializer.cs
@@ -13,11 +13,12 @@ namespace MortierFu
         public string sceneName = "MainMenu";
 
         [Expandable] public SO_GameConfig config;
-        
-        [Header("Warmup Manager")]
-        public WarmupManager warmupManager;
-        
+
+        [Header("Warmup Manager")] public WarmupManager warmupManager;
+
         private float _progress = 0f;
+
+        [Header("Debug")] public bool isPortableBootstrap = false;
 
         public float GetInitializationProgress() => _progress;
 
@@ -37,19 +38,12 @@ namespace MortierFu
         private SceneService _sceneService;
         private SaveService _saveService;
         private ShakeService _shakeService;
-        
-#if UNITY_EDITOR
-        [Header("Debug")] public bool isPortableBootstrap = false;
-#endif
-
-        //private float _progress = 0f;
 
         private void Awake()
         {
-            // Lock and hide cursor
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
-            
+
             InitializeAsync().Forget();
         }
 
@@ -70,51 +64,50 @@ namespace MortierFu
             {
                 await UniTask.Yield();
             }
+
             _progress = 0.1f;
 
             await InitializeGameService();
             _progress = 0.2f;
-            
+
             // Initialise les services de base avant les mods
             await _serviceManager.Initialize();
             _progress = 0.3f;
-            
+
             // Initialise les systèmes de base avant les mods
             await _systemManager.Initialize();
             _progress = 0.4f;
-            
-            // --- Initialize Addressables
+
+            // Initialise Addressables
             await Addressables.InitializeAsync();
             _progress = 0.5f;
-            
-            // --- Load mod resources
+
+            // Load mod resources
             await _loaderService.LoadAllModResources();
             _progress = 0.6f;
-            
-            // --- Load GameConfig banks
+
+            // Load GameConfig banks
             await _audioService.LoadBanks(config.fmodBanks);
             _progress = 0.7f;
-            
-            // --- Load mods banks TODO FIX PARCE QUE ÇA MARCHE PAS
+
+            // Load mods banks
             await _audioService.LoadBanks(_modService.GetAllModFmodBanks());
             _progress = 0.8f;
-            
-#if UNITY_EDITOR
-            // --- Check for missing services (only in editor)
+
+            // Check les services manquants
             await _serviceManager.CheckForMissingServices<IGameService>();
-            
+
             if (isPortableBootstrap)
             {
-                // Stay in current Scene
+                _sceneService.HideLoadingScreen();
                 return;
             }
-#endif
+
             await warmupManager.WarmupAllAsync();
             _progress = 0.9f;
-            
-            // --- Load MainMenu Scene
+
             await _sceneService.LoadScene(sceneName, true);
-            
+
             _progress = 1f;
             _sceneService.HideLoadingScreen();
         }
@@ -122,7 +115,7 @@ namespace MortierFu
 
         private UniTask InitializeGameService()
         {
-            // --- Instantiate core services
+            // Création des core services
             _modService = new ModService();
             _loaderService = new ModLoaderService();
             _audioService = new AudioService();
@@ -137,8 +130,8 @@ namespace MortierFu
             _saveService = new SaveService();
             _fxService = new FXService();
             _shakeService = new ShakeService();
-            
-            // --- Register services
+
+            // Register des services
             _serviceManager.Register(_modService);
             _serviceManager.Register(_loaderService);
             _serviceManager.Register(_audioService);
@@ -153,7 +146,7 @@ namespace MortierFu
             _serviceManager.Register(_sceneService);
             _serviceManager.Register(_saveService);
             _serviceManager.Register(_shakeService);
-            
+
             return UniTask.CompletedTask;
         }
 
