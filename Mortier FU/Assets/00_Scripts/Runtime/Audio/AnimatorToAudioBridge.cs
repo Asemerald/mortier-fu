64217fac7609e45b10825062b381a543f88a4fb7
BAZ Victor <v.baz@rubika-edu.com>
2026-07-02 16:46:15 +0200

    [UPDATE] FINISH CACA + PREFAB REFERENCE MUD

--- Mortier FU/Assets/00_Scripts/Runtime/Audio/AnimatorToAudioBridge.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Audio/AnimatorToAudioBridge.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class AnimatorToAudioBridge : MonoBehaviour
 {
-
-    public PlayerCharacter character;
-
+    
     public void PlayFootsteps()
     {
         AudioService.PlayOneShot(AudioService.FMODEvents.SFX_Player_Footsteps, transform.position);
