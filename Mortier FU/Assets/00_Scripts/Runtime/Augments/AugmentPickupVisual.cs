6e0408a6710ea4d796ada9b6896d64ff8346dd4b
Aseuh <tdruesne05@gmail.com>
2026-07-03 17:34:28 +0200

    Update cards

---
+++ Mortier FU/Assets/00_Scripts/Runtime/Augments/AugmentPickupVisual.cs
@@ -0,0 +1,20 @@
+using System;
+using MortierFu.Shared;
+using UnityEngine;
+
+namespace MortierFu
+{
+    // Separate visual/decoration component for augment pickups.
+    public class AugmentPickupVisual : MonoBehaviour
+    {
+        [SerializeField] private E_AugmentRarity _rarity;
+        [SerializeField] private ParticleSystem _logoParticleSystem;
+        public E_AugmentRarity Rarity => _rarity;
+
+        public void SetLogoSprite(Sprite sprite)
+        {
+            if (_logoParticleSystem == null || sprite == null) return;
+            _logoParticleSystem.textureSheetAnimation.SetSprite(0, sprite);
+        }
+    }
+}
