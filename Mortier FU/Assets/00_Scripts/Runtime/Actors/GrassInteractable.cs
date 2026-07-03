bc16e4af71e5a9531d6cda54393be53030d25b73
BAZ Victor <v.baz@rubika-edu.com>
2026-07-03 15:05:46 +0200

    [ADD] Grass can burn need retake

---
+++ Mortier FU/Assets/00_Scripts/Runtime/Actors/GrassInteractable.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Helpers.Runtime.Shading;
+using MortierFu;
+using UnityEngine;
+
+public class GrassInteractable : MonoBehaviour, IInteractable
+{
+    [SerializeField] private Renderer grassRenderer;
+    [SerializeField] private float duration;
+    
+    public bool IsDashInteractable { get; } = false;
+    public bool IsBombshellInteractable { get; private set; } = true;
+
+    private static readonly int _shaderGrassParameters = Shader.PropertyToID("_Progression");
+
+    private const float shaderGrassBurnValue = 1f;
+    private const float shaderGrassBaseValue = -0.1f;
+    
+    private CancellationTokenSource _cts;
+
+    private void Awake()
+    {
+        grassRenderer.SetFloatProperty(_shaderGrassParameters,shaderGrassBaseValue);
+        
+        _cts = new CancellationTokenSource();
+    }
+
+    public void Interact(Vector3 contactPoint)
+    {
+        Debug.Log("cc");
+        if (!IsBombshellInteractable) return;
+        Debug.Log("aa");
+        IsBombshellInteractable = false; 
+        
+        LerpShaderGrass(_cts.Token).Forget();
+    }
+
+    private async UniTask LerpShaderGrass(CancellationToken token)
+    {
+        if (duration <= 0f) 
+        {
+            grassRenderer.SetFloatProperty(_shaderGrassParameters,shaderGrassBurnValue);
+            return;
+        }
+        
+        float elapsedTime = 0f;
+        
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            
+            float ratio = Mathf.Lerp(shaderGrassBaseValue, shaderGrassBurnValue, elapsedTime / duration);
+            grassRenderer.SetFloatProperty(_shaderGrassParameters,ratio);
+            await UniTask.Yield(PlayerLoopTiming.Update,token);
+        }
+        
+        grassRenderer.SetFloatProperty(_shaderGrassParameters,shaderGrassBurnValue);
+    }
+    
+}
---
+++ Mortier FU/Assets/00_Scripts/Shared/Utility/ShaderHelper.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Helpers.Runtime.Shading
+{
+    public static class ShaderHelper
+    {
+        private static readonly MaterialPropertyBlock _propertyBlock = new();
+
+        public static void SetColorProperty(this Renderer renderer, string propertyName, Color color)
+        {
+            int id = Shader.PropertyToID(propertyName);
+            renderer.GetPropertyBlock(_propertyBlock);
+            _propertyBlock.SetColor(id, color);
+            renderer.SetPropertyBlock(_propertyBlock);
+            _propertyBlock.Clear();
+        }
+        
+        public static void SetColorProperty(this Renderer renderer, int propertyId, Color color)
+        {
+            renderer.GetPropertyBlock(_propertyBlock);
+            _propertyBlock.SetColor(propertyId, color);
+            renderer.SetPropertyBlock(_propertyBlock);
+            _propertyBlock.Clear();
+        }
+
+        public static void SetFloatProperty(this Renderer renderer, string propertyName, float value)
+        {
+            int id = Shader.PropertyToID(propertyName);
+            renderer.GetPropertyBlock(_propertyBlock);
+            _propertyBlock.SetFloat(id, value);
+            renderer.SetPropertyBlock(_propertyBlock);
+            _propertyBlock.Clear();
+        }
+
+        public static void SetFloatProperty(this Renderer renderer, int propertyId, float value)
+        {
+            renderer.GetPropertyBlock(_propertyBlock);
+            _propertyBlock.SetFloat(propertyId, value);
+            renderer.SetPropertyBlock(_propertyBlock);
+            _propertyBlock.Clear();
+        }
+    }
+}
