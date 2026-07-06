3b6d0d9d34747ac1a306bf21cffb50a3bfe750af
BAZ Victor <v.baz@rubika-edu.com>
2026-07-06 16:21:44 +0200

    [Update] Destrcutible Deseapear finally

--- Mortier FU/Assets/00_Scripts/Runtime/Actors/Breakable.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Actors/Breakable.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
@@ -13,6 +16,12 @@ namespace MortierFu
         [Space]
         [SerializeField] protected GameObject _intactMesh;
         [SerializeField] private GameObject _shatteredMesh;
+        [SerializeField] protected float durationCleanUp = 1.5f;
+
+        private readonly List<Rigidbody> _shatteredRbChildren = new();
+        private readonly List<GameObject> _shatteredMeshChildren = new();
+
+        private readonly CancellationTokenSource _cts = new();
 
         protected virtual void Awake()
         {
@@ -23,6 +32,9 @@ namespace MortierFu
                 _shatteredMesh.SetActive(false);
 
             _isIntact = true;
+            
+            _shatteredMeshChildren.Clear();
+            _shatteredRbChildren.Clear();
         }
         
         public virtual void Interact(Vector3 contactPoint)
@@ -54,13 +66,63 @@ namespace MortierFu
             // Get each shard's rigidbody
             var rigidbodies = _shatteredMesh.GetComponentsInChildren<Rigidbody>();
             
+            _shatteredRbChildren.AddRange(rigidbodies);
+            
             foreach (var rb in rigidbodies)
             {
                 rb.AddExplosionForce(_explosionForce, contactPoint, _explosionRadius, _upwardsModifier);
             }
+            
+            ShatterPiecesCleanUp().Forget();
         } 
 
         public bool IsDashInteractable => true;
         public bool IsBombshellInteractable => true;
+        
+
+        private async UniTask ShatterPiecesCleanUp()
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(2f));
+            
+            int count = _shatteredRbChildren.Count;
+            
+            float elapsedTime = 0f;
+
+            foreach (Rigidbody rb in _shatteredRbChildren)
+            {
+                _shatteredMeshChildren.Add(rb.gameObject);
+                Destroy(rb); //doesnt have any other option because of enabled doesnt exist with rb wtf ?
+            }
+            
+            while (elapsedTime < durationCleanUp)
+            {
+                elapsedTime += Time.deltaTime;
+                
+                float t = elapsedTime / durationCleanUp;
+                
+                for (int i = 0; i < count; i++)
+                {
+                    Vector3 newScale = Vector3.Lerp(
+                        _shatteredMeshChildren[i].transform.localScale,
+                        Vector3.zero,
+                        t);
+                    
+                    _shatteredMeshChildren[i].transform.localScale = newScale;
+                }
+                
+                await UniTask.Yield(PlayerLoopTiming.Update,_cts.Token);
+            }
+            
+            for (int i = 0; i < count; i++)
+                _shatteredMeshChildren[i].transform.localScale = Vector3.zero;
+            
+            //for the moment I didnt Destroy the obj for performance issue like GC spike 
+        }
+
+        private void OnDestroy()
+        {
+            _cts.Cancel();
+            _cts.Dispose();
+        }
     }   
 }
