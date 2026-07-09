536e015973a6569ee19064e43fe69e0d3326c698
BAZ Victor <v.baz@rubika-edu.com>
2026-07-09 18:14:55 +0200

    [WIP] First Iteration new UI Summary (Do Not Pull)

---
+++ Mortier FU/Assets/00_Scripts/Runtime/UI/InGame/Race/LastAugmentAnimation.cs
@@ -0,0 +1,33 @@
+﻿using Cysharp.Threading.Tasks;
+using PrimeTween;
+using UnityEngine;
+
+namespace MortierFu
+{
+    public class LastAugmentAnimation : MonoBehaviour
+    {
+        private Tween lastAugmentTween;
+        private const float scaleMultiplier = 1.5f;
+        private const float durationAnimation = 1f;
+        
+        private void OnEnable()
+        {
+            if (lastAugmentTween.isAlive)
+                lastAugmentTween.Stop();
+            
+            StartAugmentAnimation().Forget();
+        }
+
+        private void OnDisable()
+        {
+            if (lastAugmentTween.isAlive)
+                lastAugmentTween.Stop();
+        }
+
+        private async UniTaskVoid StartAugmentAnimation()
+        {
+            lastAugmentTween = Tween.Scale(transform, Vector3.one, Vector3.one * scaleMultiplier, durationAnimation, Ease.OutQuad);
+            await lastAugmentTween;
+        }
+    }
+}
