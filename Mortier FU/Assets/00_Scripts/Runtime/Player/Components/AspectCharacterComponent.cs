ee30f2d3fe5775976c1ebd7d3efcb1a2152e4508
Aseuh <tdruesne05@gmail.com>
2026-07-13 10:38:28 +0200

    fix material order

--- Mortier FU/Assets/00_Scripts/Runtime/Player/Components/AspectCharacterComponent.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/Player/Components/AspectCharacterComponent.cs
@@ -31,8 +31,8 @@ namespace MortierFu
 
     public class AspectCharacterComponent : CharacterComponent
     {
-        private const int k_playerMaterialSlot = 0;
-        private const int k_outlineMaterialSlot = 1;
+        private const int k_playerMaterialSlot = 1;
+        private const int k_outlineMaterialSlot = 0;
 
         private Sequence _blinkTween;
         private Sequence _reloadWidgetTween;
@@ -108,9 +108,10 @@ namespace MortierFu
 
         private void ApplyRuntimeMaterials()
         {
-            SetMaterialSlot(AspectMaterials.PlayerMesh, k_playerMaterialSlot, _materialInstance);
-
+            
             SetMaterialSlot(AspectMaterials.PlayerMesh, k_outlineMaterialSlot, _outlineMaterialInstance);
+            
+            SetMaterialSlot(AspectMaterials.PlayerMesh, k_playerMaterialSlot, _materialInstance);
 
             SetMaterialSlot(AspectMaterials.PlayerCrownMesh, k_outlineMaterialSlot, _outlineMaterialInstance);
 
