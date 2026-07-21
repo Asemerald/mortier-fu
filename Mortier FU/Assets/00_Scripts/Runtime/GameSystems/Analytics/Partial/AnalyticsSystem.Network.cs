ea6f7fc9f029abd9d36cde11507d47415faecbd5
Enzo Dufour <enzo.dufour05@gmail.com>
2026-07-21 19:36:34 +0200

    [UPDATE] Fix addressables

--- Mortier FU/Assets/00_Scripts/Runtime/GameSystems/Analytics/Partial/AnalyticsSystem.Network.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/GameSystems/Analytics/Partial/AnalyticsSystem.Network.cs
@@ -1,8 +1,6 @@
 using Cysharp.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.Networking;
 using System.Linq;
-using Codice.CM.WorkspaceServer.DataStore;
 using MortierFu.Shared;
 
 namespace MortierFu.Analytics
