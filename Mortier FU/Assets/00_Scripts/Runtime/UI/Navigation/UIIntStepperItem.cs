26074595ab820e57133891f4b4810ec1889b52cd
Victor Baz <v.baz@rubika-edu.com>
2026-07-28 20:51:50 +0200

    [Update] Flash Animation arrow Selection

--- Mortier FU/Assets/00_Scripts/Runtime/UI/Navigation/UIIntStepperItem.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/UI/Navigation/UIIntStepperItem.cs
@@ -3,6 +3,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using PrimeTween;
 
 namespace MortierFu
 {
@@ -12,6 +13,8 @@ namespace MortierFu
 
     public sealed class UIIntStepperItem : UINavigationItem
     {
+        #region Variables
+
         [Header("Value")]
         [SerializeField] private int _minValue = 1;
         [SerializeField] private int _maxValue = 10;
@@ -21,18 +24,29 @@ namespace MortierFu
 
         [Header("Visuals")]
         [SerializeField] private TMP_Text _valueText;
+
         [SerializeField] private Graphic _leftArrow;
         [SerializeField] private Graphic _rightArrow;
+
         [SerializeField] private Color _normalArrowColor = Color.white;
         [SerializeField] private Color _usedArrowColor = Color.green;
+
+        [SerializeField] private float _usedFlashDuration = 0.3f;
+        [SerializeField] private Ease _usedFlashEase = Ease.OutQuad;
 
         [Header("Events")]
         [SerializeField] private IntValueChangedEvent _onValueChanged;
 
         private bool _selected;
-        private int _lastDirection;
+
+        private Tween _leftArrowTween;
+        private Tween _rightArrowTween;
 
         public event Action<int> OnValueChanged;
+
+        #endregion
+
+        #region Unity LifeCycle
 
         private void Awake()
         {
@@ -41,19 +55,39 @@ namespace MortierFu
             UpdateVisuals();
         }
 
+        #endregion
+
+        #region Core
+
         public override bool HandleHorizontal(int direction)
         {
             if (direction == 0)
                 return false;
 
+            int previousValue = _currentValue;
             int nextValue = _currentValue + direction * Mathf.Max(1, _step);
 
             SetValue(nextValue, notify: true);
 
-            _lastDirection = direction;
-            UpdateVisuals();
+            if (_currentValue != previousValue)
+            {
+                if (direction < 0)
+                    FlashArrow(_leftArrow, ref _leftArrowTween);
+                else
+                    FlashArrow(_rightArrow, ref _rightArrowTween);
+            }
 
             return true;
+        }
+
+        private void FlashArrow(Graphic arrow, ref Tween tween)
+        {
+            if (!arrow)
+                return;
+
+            tween.Stop();
+            arrow.color = _usedArrowColor;
+            tween = Tween.Color(arrow, _selected ? _selectedArrowColor : _normalArrowColor, _usedFlashDuration, _usedFlashEase);
         }
 
         public void SetValue(int value, bool notify = true)
@@ -82,10 +116,6 @@ namespace MortierFu
         protected override void OnSelectionChanged(bool selected)
         {
             _selected = selected;
-
-            if (!selected)
-                _lastDirection = 0;
-
             UpdateVisuals();
         }
 
@@ -94,38 +124,16 @@ namespace MortierFu
             if (_valueText)
                 _valueText.text = _currentValue.ToString();
 
-            UpdateArrow(_leftArrow, _lastDirection < 0);
-            UpdateArrow(_rightArrow, _lastDirection > 0);
+            UpdateArrowColor(_leftArrow);
+            UpdateArrowColor(_rightArrow);
         }
 
-        private void UpdateArrow(Graphic arrow, bool used)
+        private void UpdateArrowColor(Graphic arrow)
         {
             if (!arrow)
                 return;
 
-            if (used)
-            {
-                arrow.color = _usedArrowColor;
-                return;
-            }
-
             arrow.color = _selected ? _selectedArrowColor : _normalArrowColor;
-        }
-        
-        public void ResetUsageFeedback()
-        {
-            _lastDirection = 0;
-            UpdateVisuals();
-        }
-        
-        public void ConfigureRange(int minValue, int maxValue, int step, bool wrapValue)
-        {
-            _minValue = minValue;
-            _maxValue = Mathf.Max(minValue, maxValue);
-            _step = Mathf.Max(1, step);
-            _wrapValue = wrapValue;
-
-            SetValue(_currentValue, notify: false);
         }
 
         private int WrapValue(int value)
@@ -145,6 +153,20 @@ namespace MortierFu
             return _minValue + index * step;
         }
 
+        #endregion
+
+        #region Clean & Setup
+
+        public void ConfigureRange(int minValue, int maxValue, int step, bool wrapValue)
+        {
+            _minValue = minValue;
+            _maxValue = Mathf.Max(minValue, maxValue);
+            _step = Mathf.Max(1, step);
+            _wrapValue = wrapValue;
+
+            SetValue(_currentValue, notify: false);
+        }
+
         private void ClampSettings()
         {
             if (_maxValue < _minValue)
@@ -153,5 +175,14 @@ namespace MortierFu
             if (_step <= 0)
                 _step = 1;
         }
+
+        public void ResetUsageFeedback()
+        {
+            _leftArrowTween.Stop();
+            _rightArrowTween.Stop();
+            UpdateVisuals();
+        }
+
+        #endregion
     }
 }
