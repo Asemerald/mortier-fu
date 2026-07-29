8a243c3d016856ac9c277dbbfa4905077a4d36f4
BAZ Victor <v.baz@rubika-edu.com>
2026-07-29 17:50:33 +0200

    [Update] New HUD

--- Mortier FU/Assets/00_Scripts/Runtime/UI/InGame/Player/PlayerGameplayUI.cs
+++ Mortier FU/Assets/00_Scripts/Runtime/UI/InGame/Player/PlayerGameplayUI.cs
@@ -10,37 +10,50 @@ namespace MortierFu
 {
     public class PlayerGameplayUI : MonoBehaviour
     {
-        [Header("Params")] [SerializeField] private float _healthTweenDuration = 0.18f;
+
+        #region Variables
+
+        [Header("Params")] 
+        
+        [SerializeField] private float _healthTweenDuration = 0.18f;
         [SerializeField] private float _damageFbDelay = 0.3f;
         [SerializeField] private float _damageFbDepleteDuration = 0.8f;
-
-        [Header("References")] [SerializeField]
-        private PlayerCharacter _character;
-
-        [SerializeField] private Image _healthFillImg;
-        [SerializeField] private Image _damageFillImg;
-        private Tween _damageTween;
-        private Tween _healthTween;
-        private float _damageBarWidth;
-        private float _previousDamageNormalized;
-        [SerializeField] private Image _healthTicksImg;
-        private Material _ticksMaterialInstance;
-        [SerializeField] private RectTransform _playerHUD;
-
         [SerializeField] private float _tweenDuration = 0.5f;
         [SerializeField] private float _startFadeDelay = 2f;
 
         [SerializeField] private Ease _healthBarEase = Ease.OutBack;
+        
+        [Header("References")] 
+        
+        [SerializeField] private PlayerCharacter _character;
 
+        [SerializeField] private Sprite[] backgroundPlayerColor;
+
+        [SerializeField] private Image _playerColorImg;
+        [SerializeField] private Image _healthFillImg;
+        [SerializeField] private Image _damageFillImg;
+        [SerializeField] private Image _healthTicksImg;
         [SerializeField] private Image _reloadCdImage;
-
-        [Header("Dash")] [SerializeField] private Image _strikeCdImage;
+        
+        [SerializeField] private RectTransform _playerHUD;
+        
+        [Header("Dash")] 
+        [SerializeField] private Image _strikeCdImage;
         [SerializeField] private RawImage _dashChargeTiledImg;
+        
+        private Tween _damageTween;
+        private Tween _healthTween;
+        private Tween _hudTween;
+        
+        private float _damageBarWidth;
+        private float _previousDamageNormalized;
+        
+        private Material _ticksMaterialInstance;
+
         private int _currentDashCharges = 0;
 
         private GameModeBase _gm;
 
-        private Tween _hudTween;
         private CancellationTokenSource _hudCancellation;
 
         private const int k_maxDashCharges = 7;
@@ -48,11 +61,16 @@ namespace MortierFu
         private readonly Vector3 _scaleOne = Vector3.one;
 
         private static readonly int FillID = Shader.PropertyToID("_Fill");
-        private static readonly int ScalingYID = Shader.PropertyToID("_ScalingY");
+        private static readonly int ScalingYid = Shader.PropertyToID("_ScalingY");
+        private static readonly int BlinkFactor = Shader.PropertyToID("_BlinkFactor");
 
         public bool IsIntroReady { get; private set; }
 
         public event Action<bool> OnIntroReady;
+
+        #endregion
+
+        #region Unity Lifecycle
 
         private void Awake() => _gm = GameService.CurrentGameMode as GameModeBase;
 
@@ -160,12 +178,83 @@ namespace MortierFu
             UpdateReloadUI();
             UpdateStrikeUI();
 
-            //TODO : Refacto ce bousin si possible (Eliot)
-
             if (_gm?.CurrentGameState is GameState.AugmentRace or GameState.Round or GameState.RoundCountdown)
                 ScaleHUDToAvatarSize();
         }
 
+        #endregion
+
+        #region Health
+
+        private void OnHealthChanged(float oldHealth, float newHealth)
+        {
+            float fillAmount = _character.Health.HealthRatio;
+            float maxHealth = _character.Health.MaxHealth;
+            float oldFillAmount = Mathf.Clamp01(oldHealth / maxHealth);
+
+            if (_healthTween is { isAlive: true, progress: < 1f })
+                _healthTween.Stop();
+
+            _healthTween = Tween.Custom(_healthFillImg.fillAmount, fillAmount, _healthTweenDuration, newFillAmount =>
+            {
+                _healthFillImg.fillAmount = newFillAmount;
+                _ticksMaterialInstance.SetFloat(FillID, newFillAmount);
+            }, Ease.OutQuad);
+
+            if (newHealth >= oldHealth) return;
+
+            if (_damageTween is { isAlive: true, progress: < 1f })
+                _damageTween.Stop();
+
+            _damageFillImg.fillAmount = oldFillAmount;
+            _damageTween = Tween.UIFillAmount(_damageFillImg, oldFillAmount, fillAmount, _damageFbDepleteDuration, Ease.InOutQuad, startDelay: _damageFbDelay);
+        }
+
+        private void OnMaxHealthChanged(float _)
+        {
+            float baseHealth = _character.Stats.MaxHealth.BaseValue;
+            float maxHealth = _character.Stats.MaxHealth.Value;
+
+            float delta = maxHealth - baseHealth;
+            float scalingY = 0.5f + delta / baseHealth * 0.5f;
+
+            _ticksMaterialInstance.SetFloat(ScalingYid, scalingY);
+
+            // Redraw health
+            OnHealthChanged(0f, 0f);
+        }
+
+        #endregion
+
+        #region Dash
+
+        private void OnDashChargeUpdated() => UpdateDashChargeSprite();
+
+        private void UpdateDashChargeSprite()
+        {
+            // Retrieve from character the amount of charges.
+            int dashCharges = _character.AvailableDashCharges;
+
+            // If we the amount hasn't change, no reason to change the sprite.
+            if (_currentDashCharges == dashCharges)
+                return;
+
+            _currentDashCharges = dashCharges;
+            BlinkUI(_strikeCdImage, 0.07f).Forget();
+
+            if (_character.Stats.DashCharges.Value < 2 || _currentDashCharges == 0)
+            {
+                _dashChargeTiledImg.enabled = false;
+                return;
+            }
+
+            _dashChargeTiledImg.enabled = true;
+
+            Rect rect = _dashChargeTiledImg.uvRect;
+            rect.x = 1f / k_maxDashCharges * dashCharges;
+            _dashChargeTiledImg.uvRect = rect;
+        }
+        
         private void UpdateStrikeUI()
         {
             // Reverse progress bar
@@ -178,20 +267,16 @@ namespace MortierFu
             UpdateDashChargeSprite();
         }
 
-        private void UpdateReloadUI()
-        {
-            // Reverse progress bar
-            float reloadProgress = 1 - _character.Mortar.ShootCooldownProgress;
+        #endregion
 
-            _reloadCdImage.fillAmount = reloadProgress;
-            _reloadCdImage.color = reloadProgress >= 1f ? Color.Lerp(_reloadCdImage.color, Color.white, 25 * Time.deltaTime) : new Color(0.75f, 0.75f, 0.75f, 1);
-        }
+        #region HUD
 
         private void StartShowPlayerHUD(bool showIcon)
         {
             CancelHudAnimation();
 
             _hudCancellation = new CancellationTokenSource();
+            
             ShowPlayerHUDAsync(showIcon, _hudCancellation.Token).Forget();
         }
 
@@ -199,12 +284,12 @@ namespace MortierFu
         {
             try
             {
-                await UniTask.Yield(PlayerLoopTiming.Update, cancellationToken);
-
-                ResetIntroReady();
-
                 PrepareHUDVisuals();
-
+                
+                ResetIntroReady();
+                
+                await UniTask.Yield(PlayerLoopTiming.Update, cancellationToken);
+                
                 await UniTask.Delay(TimeSpan.FromSeconds(_startFadeDelay), DelayType.UnscaledDeltaTime, PlayerLoopTiming.Update, cancellationToken);
 
                 await AnimateHUDInAsync(cancellationToken);
@@ -239,21 +324,27 @@ namespace MortierFu
         {
             if (_playerHUD)
                 _playerHUD.localScale = Vector3.zero;
+
+            InitBackgroundColorHUD();
         }
-
-        private void ResetIntroReady() => IsIntroReady = false;
-
-        private void MarkIntroReady()
+        
+        private void InitBackgroundColorHUD()
         {
-            if (IsIntroReady)
-                return;
-
-            IsIntroReady = true;
-            OnIntroReady?.Invoke(this);
+            int index = _character.Owner ? _character.Owner.PlayerIndex : 0;
+            
+            _playerColorImg.sprite = backgroundPlayerColor[index];
         }
+        
+        private void CancelHudAnimation()
+        {
+            _hudCancellation?.Cancel();
+            _hudCancellation?.Dispose();
+            _hudCancellation = null;
 
-        private void OnRaceStart() => StartShowPlayerHUD(showIcon: false);
-
+            if (_hudTween.isAlive)
+                _hudTween.Stop();
+        }
+        
         private async UniTask HidePlayerHUD()
         {
             CancelHudAnimation();
@@ -267,126 +358,7 @@ namespace MortierFu
 
             await WaitForTweenAsync(_hudTween, CancellationToken.None);
         }
-
-        private static async UniTask WaitForTweenAsync(Tween tween, CancellationToken cancellationToken)
-        {
-            while (tween.isAlive)
-            {
-                cancellationToken.ThrowIfCancellationRequested();
-                await UniTask.Yield(PlayerLoopTiming.Update, cancellationToken);
-            }
-        }
-
-        private void CancelHudAnimation()
-        {
-            _hudCancellation?.Cancel();
-            _hudCancellation?.Dispose();
-            _hudCancellation = null;
-
-            if (_hudTween.isAlive)
-                _hudTween.Stop();
-        }
         
-
-        private void OnRoundEnded(RoundInfo roundInfo) => HidePlayerHUD().Forget();
-
-        private void OnHealthChanged(float oldHealth, float newHealth)
-        {
-            var fillAmount = _character.Health.HealthRatio;
-
-            if (_healthTween is { isAlive: true, progress: < 1f })
-                _healthTween.Stop();
-
-            _healthTween = Tween.Custom(_healthFillImg.fillAmount, fillAmount, _healthTweenDuration, newFillAmount =>
-            {
-                _healthFillImg.fillAmount = newFillAmount;
-                _ticksMaterialInstance.SetFloat(FillID, newFillAmount);
-            }, Ease.OutQuad);
-
-            // Damage feedback
-            float delta = newHealth - oldHealth;
-            if (delta >= 0) return;
-
-            RectTransform damageRect = _damageFillImg.rectTransform;
-
-            float maxHealth = _character.Health.MaxHealth;
-            float oldHealthNorm = Mathf.Clamp01(oldHealth / maxHealth);
-            float newHealthNorm = Mathf.Clamp01(newHealth / maxHealth);
-
-            float leftPx = newHealthNorm * _damageBarWidth;
-
-            Vector2 offsetMin = damageRect.offsetMin;
-            offsetMin.x = leftPx;
-            damageRect.offsetMin = offsetMin;
-
-            Vector2 offsetMax = damageRect.offsetMax;
-            if (_damageTween.isAlive && _damageTween.progress < 1f)
-            {
-                _damageTween.Stop();
-                offsetMax.x -= _previousDamageNormalized * (1f - _damageFillImg.fillAmount);
-            }
-            else
-            {
-                float rightPx = (1f - oldHealthNorm) * _damageBarWidth;
-                offsetMax.x = -rightPx;
-            }
-
-            _previousDamageNormalized = _damageBarWidth - offsetMin.x + offsetMax.x;
-
-            damageRect.offsetMax = offsetMax;
-            _damageFillImg.fillAmount = 1f;
-
-            _damageTween = Tween.UIFillAmount(_damageFillImg, 1f, 0f, _damageFbDepleteDuration, Ease.InOutQuad, startDelay: _damageFbDelay);
-        }
-
-        private void OnMaxHealthChanged(float _)
-        {
-            float baseHealth = _character.Stats.MaxHealth.BaseValue;
-            float maxHealth = _character.Stats.MaxHealth.Value;
-
-            float delta = maxHealth - baseHealth;
-            float scalingY = 0.5f + delta / baseHealth * 0.5f;
-
-            _ticksMaterialInstance.SetFloat(ScalingYID, scalingY);
-
-            // Redraw health
-            OnHealthChanged(0f, 0f);
-        }
-
-        private void OnDashChargeUpdated() => UpdateDashChargeSprite();
-
-        private void UpdateDashChargeSprite()
-        {
-            // Retrieve from character the amount of charges.
-            int dashCharges = _character.AvailableDashCharges;
-
-            // If we the amount hasn't change, no reason to change the sprite.
-            if (_currentDashCharges == dashCharges)
-                return;
-
-            _currentDashCharges = dashCharges;
-            BlinkUI(_strikeCdImage, 0.07f).Forget();
-
-            if (_character.Stats.DashCharges.Value < 2 || _currentDashCharges == 0)
-            {
-                _dashChargeTiledImg.enabled = false;
-                return;
-            }
-
-            _dashChargeTiledImg.enabled = true;
-
-            Rect rect = _dashChargeTiledImg.uvRect;
-            rect.x = 1f / k_maxDashCharges * dashCharges;
-            _dashChargeTiledImg.uvRect = rect;
-        }
-
-        private async UniTask BlinkUI(Image sprite, float duration)
-        {
-            sprite.material.SetFloat("_BlinkFactor", 0.9f);
-            await UniTask.Delay(TimeSpan.FromSeconds(duration));
-            sprite.material.SetFloat("_BlinkFactor", 0);
-        }
-
         private void ScaleHUDToAvatarSize()
         {
             //TODO : Refacto ce bousin si possible (Eliot)
@@ -402,5 +374,53 @@ namespace MortierFu
             var yOffset = 1.27f - (Mathf.Abs(1 - _character.transform.localScale.x) * 0.25f);
             _playerHUD.localPosition = new Vector3(0, yOffset, 0);
         }
+
+        #endregion
+
+        #region GameState Handle
+
+        private void OnRaceStart() => StartShowPlayerHUD(showIcon: false);
+        
+        private void OnRoundEnded(RoundInfo roundInfo) => HidePlayerHUD().Forget();
+
+        #endregion
+        
+        private void UpdateReloadUI()
+        {
+            // Reverse progress bar
+            float reloadProgress = 1 - _character.Mortar.ShootCooldownProgress;
+
+            _reloadCdImage.fillAmount = reloadProgress;
+            _reloadCdImage.color = reloadProgress >= 1f ? Color.Lerp(_reloadCdImage.color, Color.white, 25 * Time.deltaTime) : new Color(0.75f, 0.75f, 0.75f, 1);
+        }
+
+        private void ResetIntroReady() => IsIntroReady = false;
+
+        private void MarkIntroReady()
+        {
+            if (IsIntroReady)
+                return;
+
+            IsIntroReady = true;
+            OnIntroReady?.Invoke(this);
+        }
+        
+        private static async UniTask WaitForTweenAsync(Tween tween, CancellationToken cancellationToken)
+        {
+            while (tween.isAlive)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                await UniTask.Yield(PlayerLoopTiming.Update, cancellationToken);
+            }
+        }
+        
+        private async UniTask BlinkUI(Image sprite, float duration)
+        {
+            sprite.material.SetFloat(BlinkFactor, 0.9f);
+            await UniTask.Delay(TimeSpan.FromSeconds(duration));
+            sprite.material.SetFloat(BlinkFactor, 0);
+        }
+
+        
     }
 }
